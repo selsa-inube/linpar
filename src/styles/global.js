726177f89ab8847d3ff6fedfffa48ce4ac554c1d
wfercanas <fernando.canas@outlook.com>
2022-12-16 16:45:36 -0500

    feat: global styles with typography

---
+++ src/styles/global.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+
+import { typography } from "./typography";
+
+const GlobalStyles = createGlobalStyle`
+
+  h1, h2, h3, h4, h5 , h6 {
+    margin: 0;
+    padding: 0
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+
+    ${typography}
+  }
+`;
+
+export { GlobalStyles };
