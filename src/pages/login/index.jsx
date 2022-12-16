e1f40a4ec2d111946f0fdfead538e3ee4a9ba8d6
wfercanas <fernando.canas@outlook.com>
2022-12-16 12:04:13 -0500

    feat: replace StyledStack for Stack component

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,8 +1,9 @@
+import { Stack } from "../../components/layout/Stack";
+
 import {
   StyledLoginContainer,
   StyledWelcomeContainer,
   StyledOutletContainer,
-  StyledStack,
   StyledHead2,
   StyledHead3,
   StyledImage,
@@ -14,17 +15,17 @@ function Login() {
   return (
     <StyledLoginContainer>
       <StyledWelcomeContainer>
-        <StyledStack>
+        <Stack>
           <StyledHead2>Bienvenido</StyledHead2>
           <StyledHead2>Portal de clientes</StyledHead2>
-        </StyledStack>
+        </Stack>
         <StyledImage src={selsaLogo} alt="Sistemas Enlinea" />
       </StyledWelcomeContainer>
       <StyledOutletContainer>
-        <StyledStack>
+        <Stack>
           <StyledHead2>Validando credenciales</StyledHead2>
           <StyledHead3>Espere un momento, por favor.</StyledHead3>
-        </StyledStack>
+        </Stack>
         <p>Spinner</p>
       </StyledOutletContainer>
     </StyledLoginContainer>
