524314e28c6ef8ef1f55b3e09dc5087a777213ab
wfercanas <fernando.canas@outlook.com>
2022-12-17 16:29:21 -0500

    feat: text component with token props

---
+++ src/components/data/Text/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { typography } from "../../../styles/typography";
+
+const StyledParagraph = styled.p`
+  font-family: ${({ token }) => typography.sys.typescale[token].font};
+  line-height: ${({ token }) => typography.sys.typescale[token].lineHeight};
+  font-size: ${({ token }) => typography.sys.typescale[token].size};
+  letter-spacing: ${({ token }) => typography.sys.typescale[token].tracking};
+  font-weight: ${({ token }) => typography.sys.typescale[token].weight};
+`;
+
+export { StyledParagraph };
