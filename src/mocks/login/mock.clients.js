a1a3e95eea793c98fb62b4adb580de3ff53de963
wfercanas <fernando.canas@outlook.com>
2022-12-19 17:22:50 -0500

    feat: mock the client service response

---
+++ src/mocks/login/mock.clients.js
@@ -0,0 +1,29 @@
+const mockClients = [
+  {
+    id: 1,
+    name: "Fondo de Empleados y Trabajadores de la Universidad del Valle",
+    logo: "https://res.cloudinary.com/wfercanas/image/upload/w_500/v1671482962/linpar/fetrabuv_tqqhyo.png",
+  },
+  {
+    id: 2,
+    name: "Cooperativa de los Trabajadores de Instituto de Seguros Sociales",
+    logo: "https://res.cloudinary.com/wfercanas/image/upload/w_500/v1671482962/linpar/cooptraiss_hu62an",
+  },
+  {
+    id: 3,
+    name: "Crediflores Cooperativa de Ahorro y Crédito",
+    logo: "https://res.cloudinary.com/wfercanas/image/upload/w_500/v1671482962/linpar/crediflores_yysoem.png",
+  },
+  {
+    id: 4,
+    name: "Fondoccidente Fondo de Empleados",
+    logo: "https://res.cloudinary.com/wfercanas/image/upload/w_500/v1671482962/linpar/fondoccidente_psx5fp",
+  },
+  {
+    id: 5,
+    name: "Presente Fondo de Empleados Grupo Éxito",
+    logo: "https://res.cloudinary.com/wfercanas/image/upload/w_500/v1671482962/linpar/presente_trxel5",
+  },
+];
+
+export { mockClients };
