25f21171d86a464d7e5f0ce8f5692b55abf4d0a9
wfercanas <fernando.canas@outlook.com>
2022-12-19 17:22:14 -0500

    feat: get text align prop

--- src/components/data/Heading/index.jsx
+++ src/components/data/Heading/index.jsx
@@ -8,12 +8,16 @@ import {
 } from "./styles";
 
 function Heading(props) {
-  const { children, level = 1, token } = props;
+  const { children, level = 1, token, align = "left" } = props;
 
   const headings = [StyledH1, StyledH2, StyledH3, StyledH4, StyledH5, StyledH6];
   const Current = headings[level - 1];
 
-  return <Current token={token}>{children}</Current>;
+  return (
+    <Current token={token} align={align}>
+      {children}
+    </Current>
+  );
 }
 
 export { Heading };
