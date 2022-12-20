8db22264e2fc35c5b4b61b1551ac940c9135c4ee
wfercanas <fernando.canas@outlook.com>
2022-12-19 23:22:17 -0500

    feat: wrap components with div and stacks,
    Also reduced font-size in titles.

---
+++ src/pages/login/outlets/CheckingCredentials/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const StyledCheckingCredentials = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+export { StyledCheckingCredentials };
