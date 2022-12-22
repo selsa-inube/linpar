0f82950b1f3cf4353827eb00cc9b2df53844225e
wfercanas <fernando.canas@outlook.com>
2022-12-22 00:26:36 -0500

    feat: control input with handleChange prop

--- src/components/inputs/Input/index.jsx
+++ src/components/inputs/Input/index.jsx
@@ -2,12 +2,17 @@ import { MdSearch } from "react-icons/md";
 import { StyledIconInput, StyledIcon, StyledInput } from "./styles";
 
 function IconInput(props) {
-  const { type = "text", value, icon, placeholder } = props;
+  const { type = "text", value, icon, placeholder, handleChange } = props;
 
   return (
     <StyledIconInput>
       <StyledIcon>{icon}</StyledIcon>
-      <StyledInput type={type} value={value} placeholder={placeholder} />
+      <StyledInput
+        type={type}
+        value={value}
+        placeholder={placeholder}
+        onChange={handleChange}
+      />
     </StyledIconInput>
   );
 }
