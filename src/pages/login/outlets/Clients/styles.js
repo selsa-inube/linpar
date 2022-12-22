e6c2cfcaf907c1f63699db68faf6db74aaa1101a
wfercanas <fernando.canas@outlook.com>
2022-12-21 23:02:52 -0500

    feat: scroll list when clients are more than 5

--- src/pages/login/outlets/Clients/styles.js
+++ src/pages/login/outlets/Clients/styles.js
@@ -1,23 +1,30 @@
 import styled from "styled-components";
 
 const StyledClients = styled.div`
-  & form {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 8px;
-    width: 500px;
-    margin: 0 auto;
-    margin-top: 32px;
-
-    @media screen and (max-width: 532px) {
-      width: auto;
-    }
-  }
-
   & button {
     margin-top: 24px;
   }
 `;
 
-export { StyledClients };
+const StyledClientsList = styled.ul`
+  list-style: none;
+  margin: 32px auto 0;
+  width: 500px;
+  max-height: 430px;
+  padding: 0px 8px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 8px;
+
+  overflow-y: ${({ scroll }) => (scroll ? "scroll" : "visible")};
+
+  @media screen and (max-width: 532px) {
+    width: auto;
+  }
+`;
+
+const StyledClientsItem = styled.li``;
+
+export { StyledClients, StyledClientsList, StyledClientsItem };
