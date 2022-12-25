047789b64b7917561d51eadfc48ff91702f0e587
wfercanas <fernando.canas@outlook.com>
2022-12-25 11:56:14 -0500

    feat: add sigla property to mockClients

--- src/mocks/login/mock.clients.js
+++ src/mocks/login/mock.clients.js
@@ -2,56 +2,67 @@ const mockClients = [
   {
     id: 1,
     name: "Fondo de Empleados y Trabajadores de la Universidad del Valle",
+    sigla: "Fonvalle",
     logo: "https://res.cloudinary.com/wfercanas/image/upload/w_500/v1671482962/linpar/fetrabuv_tqqhyo.png",
   },
   {
     id: 2,
     name: "Cooperativa de los Trabajadores de Instituto de Seguros Sociales",
+    sigla: "Cooptraiss",
     logo: "https://res.cloudinary.com/wfercanas/image/upload/w_500/v1671482962/linpar/cooptraiss_hu62an",
   },
   {
     id: 3,
     name: "Crediflores Cooperativa de Ahorro y Crédito",
+    sigla: "Crediflores",
     logo: "https://res.cloudinary.com/wfercanas/image/upload/w_500/v1671482962/linpar/crediflores_yysoem.png",
   },
   {
     id: 4,
     name: "Fondoccidente Fondo de Empleados",
+    sigla: "Fondoccidente",
     logo: "https://res.cloudinary.com/wfercanas/image/upload/w_500/v1671482962/linpar/fondoccidente_psx5fp",
   },
   {
     id: 5,
     name: "Presente Fondo de Empleados Grupo Éxito",
+    sigla: "Presente",
     logo: "https://res.cloudinary.com/wfercanas/image/upload/w_500/v1671482962/linpar/presente_trxel5",
   },
   {
     id: 6,
     name: "Fondoccidente Fondo de Empleados",
+    sigla: "Fondoccidente",
     logo: "https://res.cloudinary.com/wfercanas/image/upload/w_500/v1671482962/linpar/fondoccidente_psx5fp",
   },
   {
     id: 7,
     name: "Presente Fondo de Empleados Grupo Éxito",
+    sigla: "Presente",
     logo: "https://res.cloudinary.com/wfercanas/image/upload/w_500/v1671482962/linpar/presente_trxel5",
   },
   {
     id: 8,
     name: "Fondo de Empleados y Trabajadores de la Universidad del Valle",
+    sigla: "Fonvalle",
     logo: "https://res.cloudinary.com/wfercanas/image/upload/w_500/v1671482962/linpar/fetrabuv_tqqhyo.png",
   },
   {
     id: 9,
     name: "Cooperativa de los Trabajadores de Instituto de Seguros Sociales",
+    sigla: "Cooptraiss",
     logo: "https://res.cloudinary.com/wfercanas/image/upload/w_500/v1671482962/linpar/cooptraiss_hu62an",
   },
   {
     id: 10,
     name: "Crediflores Cooperativa de Ahorro y Crédito",
+    sigla: "Crediflores",
     logo: "https://res.cloudinary.com/wfercanas/image/upload/w_500/v1671482962/linpar/crediflores_yysoem.png",
   },
   {
     id: 11,
     name: "Fondoccidente Fondo de Empleados",
+    sigla: "Fondoccidente",
     logo: "https://res.cloudinary.com/wfercanas/image/upload/w_500/v1671482962/linpar/fondoccidente_psx5fp",
   },
 ];
