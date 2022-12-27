2db1e47fc4032c18116e999f0048c816fa76398f
wfercanas <fernando.canas@outlook.com>
2022-12-27 15:04:02 -0500

    feat: avatar component for app header

---
+++ src/components/Avatar/styles.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+import { colors } from "../../styles/colors";
+
+const StyledAvatar = styled.div`
+  box-sizing: border-box;
+  padding: 8px 16px;
+  border-left: 1px solid ${colors.ref.palette.lightNeutral.ln400};
+  cursor: pointer;
+`;
+
+const StyledAvatarText = styled.div`
+  @media screen and (max-width: 450px) {
+    display: none;
+  }
+`;
+
+const StyledAvatarIcon = styled.img`
+  width: 32px;
+  height: 100%;
+`;
+
+export { StyledAvatar, StyledAvatarText, StyledAvatarIcon };
