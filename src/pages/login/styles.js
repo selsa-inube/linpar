7be051085026ba0aa2481512464dc8ef5d1f8a7d
wfercanas <fernando.canas@outlook.com>
2022-12-27 15:53:20 -0500

    feat: reduce gray area in portait orientation

--- src/pages/login/styles.js
+++ src/pages/login/styles.js
@@ -9,7 +9,7 @@ const StyledLoginContainer = styled.div`
 
   @media screen and (max-width: 1000px) {
     grid-template-columns: 1fr;
-    grid-template-rows: minmax(200px, 30vh) 1fr;
+    grid-template-rows: minmax(150px, 20vh) 1fr;
   }
 `;
 
@@ -22,7 +22,7 @@ const StyledWelcomeContainer = styled.div`
   text-align: center;
   gap: 32px;
 
-  @media screen and (max-width: 600px) {
+  @media screen and (max-width: 1000px) {
     gap: 16px;
   }
 `;
