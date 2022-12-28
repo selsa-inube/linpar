d201954a44e7e159b68fe9546a12f55b2b5f6659
wfercanas <fernando.canas@outlook.com>
2022-12-28 15:19:45 -0500

    feat: mock list of apps to be rendered in homepage

---
+++ src/mocks/home/apps.mock.js
@@ -0,0 +1,60 @@
+import { MdVpnKey } from "react-icons/md";
+
+const mockApps = [
+  {
+    id: 1,
+    label: "Privilegios",
+    description: "Modifica las propiedades y permisos de tu cuenta",
+    icon: <MdVpnKey />,
+  },
+  {
+    id: 2,
+    label: "Privilegios",
+    description: "Modifica las propiedades y permisos de tu cuenta",
+    icon: <MdVpnKey />,
+  },
+  {
+    id: 3,
+    label: "Privilegios",
+    description: "Modifica las propiedades y permisos de tu cuenta",
+    icon: <MdVpnKey />,
+  },
+  {
+    id: 4,
+    label: "Privilegios",
+    description: "Modifica las propiedades y permisos de tu cuenta",
+    icon: <MdVpnKey />,
+  },
+  {
+    id: 5,
+    label: "Privilegios",
+    description: "Modifica las propiedades y permisos de tu cuenta",
+    icon: <MdVpnKey />,
+  },
+  {
+    id: 6,
+    label: "Privilegios",
+    description: "Modifica las propiedades y permisos de tu cuenta",
+    icon: <MdVpnKey />,
+  },
+  {
+    id: 7,
+    label: "Privilegios",
+    description: "Modifica las propiedades y permisos de tu cuenta",
+    icon: <MdVpnKey />,
+  },
+  {
+    id: 8,
+    label: "Privilegios",
+    description: "Modifica las propiedades y permisos de tu cuenta",
+    icon: <MdVpnKey />,
+  },
+  {
+    id: 9,
+    label: "Privilegios",
+    description: "Modifica las propiedades y permisos de tu cuenta",
+    icon: <MdVpnKey />,
+  },
+];
+
+export { mockApps };
