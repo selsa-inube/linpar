1050aef25b815b5e4a54b3157e7e9c7b706530a0
wfercanas <fernando.canas@outlook.com>
2022-12-28 15:19:28 -0500

    feat: app card component for homepage

---
+++ src/components/cards/AppCard/styles.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+import { colors } from "../../../styles/colors";
+
+const StyledAppCard = styled.li`
+  box-sizing: border-box;
+  padding: 24px;
+  min-height: 180px;
+  width: 250px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  border-radius: 4px;
+  border: 1px solid ${colors.ref.palette.lightNeutral.ln200};
+  box-shadow: 3px 3px 5px 1px ${colors.ref.palette.lightNeutral.ln200};
+  cursor: pointer;
+
+  &:hover {
+    color: ${colors.sys.actions.primary};
+    background-color: ${colors.sys.actions.secondary};
+    box-shadow: none;
+  }
+`;
+
+const StyledIcon = styled.i`
+  svg {
+    width: 24px;
+    height: 24px;
+  }
+`;
+
+export { StyledAppCard, StyledIcon };
