2b0bbea61c0a3473e1b17ab2e4cfc08978f31728
Johan Daniel Garcia Nova <gtahghfsg@gmail.com>
2023-01-25 17:51:46 -0500

    feat(header): added hamburger menu icon for resolutions under 851px

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,13 @@
+import React from "react";
 import { Avatar } from "../Avatar";
-
 import { StyledHeader, StyledLogo } from "./styles";
-
+import { FaBars } from "react-icons/fa";
 import linparLogo from "../../assets/images/linpar.png";
 
 function Header() {
   return (
     <StyledHeader>
+      <FaBars className="menu-icon" size={25} />
       <StyledLogo src={linparLogo} alt="LinPar" />
       <Avatar />
     </StyledHeader>
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,18 +1,25 @@
 import styled from "styled-components";
-
 import { colors } from "../../styles/colors";
 
 const StyledHeader = styled.header`
   max-height: 56px;
   display: grid;
-  grid-template-columns: 1fr auto;
+  grid-template-columns: auto 1fr auto;
   align-items: center;
-  padding: 0px 0px 0px 16px;
+  padding: 0 16px;
   border-bottom: 1px solid ${colors.ref.palette.lightNeutral.ln400};
+
+  @media (min-width: 851px) {
+    grid-template-columns: 1fr auto;
+    .menu-icon {
+      display: none;
+    }
+  }
 `;
 
 const StyledLogo = styled.img`
   max-width: 100px;
+  margin-left: 20px;
 `;
 
 export { StyledHeader, StyledLogo };
