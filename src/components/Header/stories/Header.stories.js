8cc5024a1292a3a43d56fbf3cf8e5430bf26001d
Johan Daniel Garcia Nova <gtahghfsg@gmail.com>
2023-01-26 00:28:26 -0500

    feat(Header): create story for Storybook

---
+++ src/components/Header/stories/Header.stories.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { Header } from "../index";
+
+storiesOf("components/Header", module).add("default", () => <Header />);
