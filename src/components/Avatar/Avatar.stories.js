33aa102439c6991939f375ede037f9b26d8291b8
angie <angiepinillanova@gmail.com>
2023-01-27 09:13:04 -0500

    feat(avatar): story creation of the avatar component in the storybook

---
+++ src/components/Avatar/Avatar.stories.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Avatar } from "./index";
+
+const story = {
+  components: [Avatar],
+  title: "components/Avatar",
+};
+
+const Default = () => <Avatar />;
+
+export default story;
+
+export { Default };
