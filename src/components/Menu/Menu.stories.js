68c6a228e8c2dca009f2043217f3bdc745765b30
Johan Daniel Garcia Nova <gtahghfsg@gmail.com>
2023-02-01 11:04:49 -0500

    feat(menu): created storybook

---
+++ src/components/Menu/Menu.stories.js
@@ -0,0 +1,17 @@
+import { Menu } from "../Menu";
+import { mockApps } from "../../mocks/home/apps.mock";
+
+const story = {
+  component: [Menu],
+  title: "components/Menu",
+};
+
+const Template = (args) => <Menu {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  apps: mockApps,
+  title: "Menú",
+};
+
+export default story;
