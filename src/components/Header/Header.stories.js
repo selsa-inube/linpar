8235fc85b60a5327c432e78d29a802a75a2126d9
angie <angiepinillanova@gmail.com>
2023-02-01 17:42:15 -0500

    refactor(header): add storybook args to pass demo props

--- src/components/Header/Header.stories.js
+++ src/components/Header/Header.stories.js
@@ -1,11 +1,17 @@
 import { Header } from "../Header";
+import linparLogo from "../../assets/images/linpar.png";
 
 const story = {
   components: [Header],
   title: "components/Header",
 };
 
-const Default = () => <Header />;
+const Default = (args) => <Header {...args} />;
+Default.args = {
+  username: "Leonardo Garzón",
+  businessName: "Fondoccidente",
+  appLogo: linparLogo,
+};
 
 export default story;
 
