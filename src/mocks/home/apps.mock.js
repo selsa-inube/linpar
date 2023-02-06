005ca062c5e1f7de82ed9cd637b0c6b23f4bf29a
Cristian Rojas <crisrouges47@gmail.com>
2023-02-06 12:13:04 -0500

    fix(appsMock): adjust records to be different

--- src/mocks/home/apps.mock.js
+++ src/mocks/home/apps.mock.js
@@ -1,4 +1,14 @@
-import { MdVpnKey } from "react-icons/md";
+import {
+  MdVpnKey,
+  MdMoney,
+  MdContacts,
+  MdSupervisedUserCircle,
+  MdFolder,
+  MdOutlineDesignServices,
+  MdInventory,
+  MdLineStyle,
+  MdTableView,
+} from "react-icons/md";
 
 const mockApps = [
   {
@@ -9,51 +19,53 @@ const mockApps = [
   },
   {
     id: 2,
-    label: "Privilegios",
-    description: "Modifica las propiedades y permisos de tu cuenta",
-    icon: <MdVpnKey />,
+    label: "Contabilidad",
+    description: "Registra ingresos y gastos económicos de la compañía",
+    icon: <MdMoney />,
   },
   {
     id: 3,
-    label: "Privilegios",
-    description: "Modifica las propiedades y permisos de tu cuenta",
-    icon: <MdVpnKey />,
+    label: "Contactos",
+    description: "Registra tus contactos y almacénalos de manera óptima",
+    icon: <MdContacts />,
   },
   {
     id: 4,
-    label: "Privilegios",
-    description: "Modifica las propiedades y permisos de tu cuenta",
-    icon: <MdVpnKey />,
+    label: "CRM",
+    description:
+      "Administra las relaciones con tus clientes, buscando su satisfacción",
+    icon: <MdSupervisedUserCircle />,
   },
   {
     id: 5,
-    label: "Privilegios",
-    description: "Modifica las propiedades y permisos de tu cuenta",
-    icon: <MdVpnKey />,
+    label: "Documentos",
+    description: "Administra toda la documentación de tu compañía",
+    icon: <MdFolder />,
   },
   {
     id: 6,
-    label: "Privilegios",
-    description: "Modifica las propiedades y permisos de tu cuenta",
-    icon: <MdVpnKey />,
+    label: "Marketing",
+    description:
+      "Crea campañas para la presentación de tus productos o servicios",
+    icon: <MdOutlineDesignServices />,
   },
   {
     id: 7,
-    label: "Privilegios",
-    description: "Modifica las propiedades y permisos de tu cuenta",
-    icon: <MdVpnKey />,
+    label: "Inventario",
+    description: "Relaciona detalladamente los bienes de tu compañía",
+    icon: <MdInventory />,
   },
   {
     id: 8,
-    label: "Privilegios",
-    description: "Modifica las propiedades y permisos de tu cuenta",
-    icon: <MdVpnKey />,
+    label: "Flujos de trabajo",
+    description: "Estandariza las tareas que se realizan en tu compañía",
+    icon: <MdLineStyle />,
   },
   {
     id: 9,
-    label: "Privilegios",
-    description: "Modifica las propiedades y permisos de tu cuenta",
-    icon: <MdVpnKey />,
+    label: "Gestión de proyectos",
+    description: "Administra los proyectos establecidos para cumplir objetivos",
+    icon: <MdTableView />,
   },
 ];
 
