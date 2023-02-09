cce35a67db666296e7cf154b59e16879210c459a
angie <angiepinillanova@gmail.com>
2023-02-09 18:16:14 -0500

    fix(appMenuCard): remove Link from index.jsx and place it in styles.js

--- src/components/cards/AppMenuCard/index.jsx
+++ src/components/cards/AppMenuCard/index.jsx
@@ -1,25 +1,20 @@
-import { Link } from "react-router-dom";
 import { Text } from "../../data/Text";
 import { Stack } from "../../layout/Stack";
-
 import { StyledAppMenuCard, StyledIcon } from "./styles";
 
 function AppMenuCard(props) {
   const { icon, label, description, url } = props;
-
   return (
-    <Link to={url}>
-      <StyledAppMenuCard>
-        <Stack spacing="4" align="center">
-          <StyledIcon> {icon} </StyledIcon>
-          <Text align="center" typoToken="titleLarge">
-            {" "}
-            {label}{" "}
-          </Text>
-          <Text align="center"> {description} </Text>
-        </Stack>
-      </StyledAppMenuCard>
-    </Link>
+    <StyledAppMenuCard to={url}>
+      <Stack spacing="4" align="center">
+        <StyledIcon> {icon} </StyledIcon>
+        <Text align="center" typoToken="titleLarge">
+          {" "}
+          {label}{" "}
+        </Text>
+        <Text align="center"> {description} </Text>
+      </Stack>
+    </StyledAppMenuCard>
   );
 }
 
--- src/components/cards/AppMenuCard/styles.js
+++ src/components/cards/AppMenuCard/styles.js
@@ -1,13 +1,15 @@
 import styled from "styled-components";
 import { colors } from "../../../styles/colors";
+import { Link } from "react-router-dom";
 
-const StyledAppMenuCard = styled.div`
+const StyledAppMenuCard = styled(Link)`
   display: flex;
   justify-content: center;
   padding: 16px;
   width: 191px;
   height: 135px;
-
+  text-decoration: none;
+  color: ${colors.ref.palette.neutral.n900};
   :hover {
     & svg {
       color: ${colors.sys.actions.primary};
