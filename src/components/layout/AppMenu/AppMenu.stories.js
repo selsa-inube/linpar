81f8d1afb7f6258899e1ec94f587e2a6c00fae80
angie <angiepinillanova@gmail.com>
2023-02-17 17:30:52 -0500

    docs(appMenu): story creation in the storybook

---
+++ src/components/layout/AppMenu/AppMenu.stories.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { AppMenu } from "./index";
+import { mockApps } from "../../../mocks/home/apps.mock";
+import { mockPrivilegeOptions } from "../../../mocks/apps/privileges.mock";
+import { BrowserRouter } from "react-router-dom";
+
+const story = {
+  components: [AppMenu],
+  title: "layouts/AppMenu",
+  decorators: [
+    (Story) => (
+      <BrowserRouter>
+        <Story />
+      </BrowserRouter>
+    ),
+  ],
+};
+
+const Privileges = (args) => <AppMenu {...args} />;
+Privileges.args = {
+  appName: mockApps[0].label,
+  appDescription: mockApps[0].description,
+  appIcon: mockApps[0].icon,
+  appOptions: mockPrivilegeOptions,
+};
+
+export default story;
+
+export { Privileges };
