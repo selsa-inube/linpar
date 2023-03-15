a0ce74e68e19843ce65036f514a8776324ebdaf2
angie <angiepinillanova@gmail.com>
2023-03-15 11:23:53 -0500

    fix(table): change to get the values of the records of each page in pagination

--- src/components/data/Table/index.jsx
+++ src/components/data/Table/index.jsx
@@ -11,46 +11,35 @@ function Table(props) {
     pageLength = 10,
   } = props;
 
-  const [currentPage, setCurrentPage] = useState(1);
-  const [entriesIndex, setEntriesIndex] = useState(0);
-
-  const totalPages = Math.ceil(entries.length / pageLength);
+  const [currentPage, setCurrentPage] = useState(1),
+    totalPages = Math.ceil(entries.length / pageLength),
+    numRecordMax = pageLength * currentPage,
+    numRecordsPerPage = numRecordMax - pageLength,
+    firstEntryInPage = numRecordsPerPage + 1,
+    EndEntryInPage = getPageEntries().length + numRecordsPerPage;
 
   function getPageEntries() {
-    const numRecordMax = pageLength * currentPage;
-    return entries.slice(numRecordMax - pageLength, numRecordMax);
+    return entries.slice(numRecordsPerPage, numRecordMax);
   }
 
   function goToFirstPage() {
     setCurrentPage(1);
-    setEntriesIndex(0);
   }
 
   function goToEndPage() {
     setCurrentPage(totalPages);
-    setEntriesIndex((totalPages - 1) * pageLength);
   }
 
   function nextPage() {
     if (currentPage !== totalPages) {
       setCurrentPage(currentPage + 1);
-      setEntriesIndex(entriesIndex + pageLength);
     }
   }
 
   function prevPage() {
     if (currentPage > 1) {
       setCurrentPage(currentPage - 1);
-      setEntriesIndex(entriesIndex - pageLength);
     }
-  }
-
-  function numRecordsEnd() {
-    return getPageEntries().length + entriesIndex;
-  }
-
-  function numRecordsFirst() {
-    return entriesIndex + 1;
   }
 
   return (
@@ -58,8 +47,8 @@ function Table(props) {
       <TableUI titles={titles} actions={actions} entries={getPageEntries()} />
       {entries.length > pageLength && (
         <Pagination
-          valueDataFirst={numRecordsFirst()}
-          valueDataEnd={numRecordsEnd()}
+          valueDataFirst={firstEntryInPage}
+          valueDataEnd={EndEntryInPage}
           totalRecords={entries.length}
           handleStartPage={goToFirstPage}
           handlePrevPage={prevPage}
