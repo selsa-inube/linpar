e8838005e569b54e8d2aebd3254d5737938068da
Cristian Rojas <crisrouges47@gmail.com>
2023-03-17 11:46:47 -0500

    fix(userspage): adjust input from page

--- src/pages/privileges/outlets/users/styles.js
+++ src/pages/privileges/outlets/users/styles.js
@@ -3,6 +3,10 @@ import styled from "styled-components";
 const StyledContainer = styled.div`
   padding: 32px 64px;
 
+  div > div:last-of-type > div:nth-of-type(2) {
+    gap: 8px;
+  }
+
   div > div:last-of-type > div:nth-of-type(2) > div {
     max-width: 280px;
   }
