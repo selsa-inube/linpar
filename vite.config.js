3df731d7f7c12d4c9d658e0d443f20f2309b3717
Johan Daniel Garcia Nova <gtahghfsg@gmail.com>
2023-05-04 07:53:43 -0500

    fix(vite.config.js): adjust open property for auto open on browser

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(() => {
-  return {
-    build: {
-      outDir: "build",
-    },
-    plugins: [react()],
-  };
+export default defineConfig({
+  build: {
+    outDir: "build",
+  },
+  plugins: [react()],
+  server: {
+    open: true,
+  },
 });
