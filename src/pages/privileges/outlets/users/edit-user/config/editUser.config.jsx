7f44bbc01aaaf3ec37af81d243a8a62e63b07454
Sebastian Lara <jsebastianlara3245@gmail.com>
2023-05-17 08:56:32 -0500

    refactor(editUsers): change icon page

--- src/pages/privileges/outlets/users/edit-user/config/editUser.config.jsx
+++ src/pages/privileges/outlets/users/edit-user/config/editUser.config.jsx
@@ -1,12 +1,12 @@
-import { MdPerson } from "react-icons/md";
+import { MdModeEdit } from "react-icons/md";
 
 const editUserOptionsConfig = [
   {
     id: 1,
-    icon: <MdPerson />,
+    icon: <MdModeEdit />,
     label: "User edition",
     description: "Edit user properties and privileges",
-    url: "/privileges/users/user-edition",
+    url: "/privileges/users/user edition",
   },
 ];
 
