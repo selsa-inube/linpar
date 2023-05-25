3124d004038aba7e7d45c43994e2172dfd807172
Johan Daniel Garcia Nova <gtahghfsg@gmail.com>
2023-05-24 21:09:33 -0500

    feat: added SubjectData functionality

--- src/pages/privileges/outlets/users/complete-register/interface.jsx
+++ src/pages/privileges/outlets/users/complete-register/interface.jsx
@@ -6,22 +6,39 @@ import {
 } from "./config/completeRegister.config";
 import { StyledContainer } from "./styles";
 import { Assisted } from "../../../../../components/feedback/Assisted";
+import { SubjectCard } from "../../../../../components/cards/SubjectCard";
+import { invitationEntriesDataMock } from "../../../../../mocks/apps/privileges/invitations.mock";
 
 function CompleteRegisterUI(props) {
   const { id } = props;
 
   const smallScreen = useMediaQuery("(max-width: 580px)");
 
+  const foundEntry = invitationEntriesDataMock.find((entry) => entry.id == id);
+
+  const subjectCardData = {
+    Nombre: foundEntry.username,
+    Identificación: foundEntry.userID,
+    Correo: foundEntry.mail,
+    Invitación: foundEntry.invitationDate,
+  };
+
   return (
     <StyledContainer smallScreen={smallScreen}>
       <Stack gap="48px" direction="column">
         <Stack gap="32px" direction="column">
           <Breadcrumbs route={completeRegisterUserConfig[0].route} />
-          <PageTitle
-            title={completeRegisterUserConfig[0].title}
-            icon={completeRegisterUserConfig[0].icon}
-            description={completeRegisterUserConfig[0].description}
-          />
+          <Stack justifyContent="space-between" alignItems="center" gap="49px">
+            <PageTitle
+              title={completeRegisterUserConfig[0].title}
+              icon={completeRegisterUserConfig[0].icon}
+              description={completeRegisterUserConfig[0].description}
+            />
+            <SubjectCard
+              subjectData={subjectCardData}
+              title="Informacion del usuario"
+            ></SubjectCard>
+          </Stack>
         </Stack>
         <Assisted steps={stepsRegisterUserConfig}></Assisted>
       </Stack>
