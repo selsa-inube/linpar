f7f3a2bd0d667deedef8f26ae809583566912b61
Juan Sebastian Lara <jsebastianlara3245@gmail.com>
2023-05-29 18:06:34 -0500

    fix(branchesForm): error for submit and change functions

--- src/pages/privileges/outlets/users/edit-user/forms/BranchesForm/BranchesForm.stories.jsx
+++ src/pages/privileges/outlets/users/edit-user/forms/BranchesForm/BranchesForm.stories.jsx
@@ -1,4 +1,5 @@
 import { branchesEditUser } from "@mocks/apps/privileges/branchesForm.mock";
+import { action } from "@storybook/addon-actions";
 import { BrowserRouter } from "react-router-dom";
 import { BranchesForm } from "./index";
 
@@ -16,10 +17,20 @@ const story = {
 
 const Template = (args) => <BranchesForm {...args} />;
 
+const handleChange = (branches) => {
+  action("Branches changes: ")(branches);
+};
+
+const handleSubmit = (branches) => {
+  action("Branches submited: ")(branches);
+};
+
 export const Default = Template.bind({});
 Default.args = {
   allowSubmit: true,
   currentBranches: branchesEditUser,
+  handleChange,
+  handleSubmit,
 };
 
 export default story;
