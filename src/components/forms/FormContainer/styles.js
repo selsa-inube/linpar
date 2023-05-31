e045d90f2a3858dd7baa46ec4f069de3bf5aa04a
Cristian Rojas <crisrouges47@gmail.com>
2023-05-31 16:39:02 -0500

    fix(usereditform): sort files

--- src/components/forms/FormContainer/styles.js
+++ src/components/forms/FormContainer/styles.js
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import { colors } from "../../../styles/colors";
 
 const StyledFormContainer = styled.div`
   width: 100%;
@@ -13,25 +12,4 @@ const StyledFormContainer = styled.div`
   }
 `;
 
-const StyledSelect = styled.select`
-  display: grid;
-  align-items: center;
-  box-sizing: border-box;
-  border-radius: 8px;
-  user-select: none;
-  font-family: Roboto;
-  font-size: 16px;
-  font-weight: 400;
-  line-height: 24px;
-  color: ${colors.sys.text.dark};
-  background-color: ${colors.ref.palette.neutral.n10};
-  border-color: ${colors.ref.palette.neutral.n40};
-  padding-left: 16px;
-  height: 42px;
-
-  :focus {
-    border: 1px solid ${colors.ref.palette.blue.b400};
-  }
-`;
-
-export { StyledFormContainer, StyledSelect };
+export { StyledFormContainer };
--- src/pages/privileges/outlets/users/edit-user/forms/GeneralInfoForm/interface.jsx
+++ src/pages/privileges/outlets/users/edit-user/forms/GeneralInfoForm/interface.jsx
@@ -1,7 +1,9 @@
 import { FormContainer } from "@components/forms/FormContainer";
-import { Button, Stack } from "@inube/design-system";
+import { StyledSelect } from "./styles";
+import { Button, Stack, TextField, Text } from "@inube/design-system";
 import { SectionMessage } from "@components/feedback/SectionMessage";
 import { messageGeneralInfoConfig } from "./config/messageGeneralInfoConfig";
+import { MdOutlineModeEdit } from "react-icons/md";
 
 function GeneralInformationFormUI(props) {
   const {
@@ -58,34 +60,129 @@ function GeneralInformationFormUI(props) {
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <FormContainer
-          handleFieldChange={handleFieldChange}
-          user={user}
-          isLoading={isLoading}
-          formInvalid={formInvalid}
-          runValidations={runValidations}
-          isFieldModified={isFieldModified}
-        />
-        {allowSubmit && (
-          <Stack gap="8px" justifyContent="flex-end">
-            <Button
-              appearance="secondary"
-              type="reset"
-              handleClick={resetValues}
-              isDisabled={handleButtons}
+        <FormContainer>
+          <TextField
+            label="Nombre"
+            placeholder="Enter your full name"
+            name="name"
+            id="name"
+            type="text"
+            size="compact"
+            isFullWidth={true}
+            value={user.name.value}
+            handleChange={(event) => handleFieldChange(event, "name")}
+            isDisabled={isFieldModified("name")}
+            iconAfter={
+              user.name.value !== "" ? "" : <MdOutlineModeEdit size={18} />
+            }
+            isInvalid={user.name.isInvalid && formInvalid}
+            errorMessage="Este campo no puede estar vacío"
+            validMessage="El correo electrónico es valido"
+            handleBlur={runValidations}
+            state={user.name.state}
+          />
+
+          <TextField
+            label="Identificación"
+            placeholder="Enter your identification number"
+            name="identification"
+            id="identification"
+            type="number"
+            size="compact"
+            isFullWidth={true}
+            value={user.identification.value}
+            handleChange={(event) => handleFieldChange(event, "identification")}
+            isDisabled={isFieldModified("identification")}
+            iconAfter={
+              user.identification.value !== "" ? (
+                ""
+              ) : (
+                <MdOutlineModeEdit size={18} />
+              )
+            }
+            isInvalid={user.identification.isInvalid && formInvalid}
+            errorMessage="Este campo no puede estar vacío"
+            validMessage="El correo electrónico es valido"
+            handleBlur={runValidations}
+            state={user.identification.state}
+          />
+
+          <TextField
+            label="Correo"
+            placeholder="Enter your email address"
+            name="email"
+            id="email"
+            type="email"
+            size="compact"
+            value={user.email.value}
+            handleChange={(event) => handleFieldChange(event, "email")}
+            isFullWidth={true}
+            iconAfter={<MdOutlineModeEdit size={18} />}
+            isDisabled={isLoading && true}
+            isInvalid={user.email.isInvalid && formInvalid}
+            errorMessage="Este campo no puede estar vacío"
+            validMessage="El correo electrónico es valido"
+            handleBlur={runValidations}
+            state={user.email.state}
+          />
+
+          <TextField
+            label="Teléfono"
+            placeholder="Enter your phone number"
+            name="number"
+            id="number"
+            type="number"
+            size="compact"
+            value={user.number.value}
+            handleChange={(event) => handleFieldChange(event, "number")}
+            isFullWidth={true}
+            iconAfter={<MdOutlineModeEdit size={18} />}
+            isDisabled={isLoading && true}
+            isInvalid={user.number.isInvalid && formInvalid}
+            errorMessage="Este campo no puede estar vacío o no contener 10 digitos"
+            validMessage="El número de teléfono es valido"
+            handleBlur={runValidations}
+            state={user.number.state}
+          />
+
+          <Stack direction="column" gap="8px">
+            <Text typo="labelMedium" padding="0px 0px 0px 16px">
+              Cargo
+            </Text>
+            <StyledSelect
+              value={user.rol.value}
+              onChange={(event) => handleFieldChange(event, "rol")}
+              disabled={isLoading && true}
+              isInvalid={user.rol.isInvalid && formInvalid}
+              handleBlur={runValidations}
             >
-              Cancelar
-            </Button>
-            <Button
-              type="submit"
-              appearance={"confirm"}
-              isLoading={isLoading}
-              isDisabled={handleButtons}
-            >
-              Guardar
-            </Button>
+              <option value="Diseñador">Diseñador</option>
+              <option value="Desarrollador">Desarrollador Web</option>
+              <option value="Product Manager">Product Manager</option>
+              <option value=""></option>
+            </StyledSelect>
           </Stack>
-        )}
+          {allowSubmit && (
+            <Stack gap="8px" justifyContent="flex-end">
+              <Button
+                appearance="secondary"
+                type="reset"
+                handleClick={resetValues}
+                isDisabled={handleButtons}
+              >
+                Cancelar
+              </Button>
+              <Button
+                type="submit"
+                appearance={"confirm"}
+                isLoading={isLoading}
+                isDisabled={handleButtons}
+              >
+                Guardar
+              </Button>
+            </Stack>
+          )}
+        </FormContainer>
       </form>
       {renderMessages()}
     </>
