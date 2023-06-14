239687909713ae489a700b5d04d9238941531017
Johan Daniel Garcia Nova <gtahghfsg@gmail.com>
2023-06-14 13:42:46 -0500

    feat: events form insert

--- src/pages/privileges/outlets/users/edit-user/index.jsx
+++ src/pages/privileges/outlets/users/edit-user/index.jsx
@@ -1,8 +1,13 @@
-import { useState } from "react";
-import { EditUserUI } from "./interface";
-import { editUserTabsConfig } from "./config/editUserTabs.config";
+import { aidBudgetsFormEditUser } from "@mocks/apps/privileges/aidBudgetsForm.mock";
+import { branchesFormEditUser } from "@mocks/apps/privileges/branchesForm.mock";
+import { eventsFormEditUser } from "@mocks/apps/privileges/eventsForm.mock";
+import { payrollsFormEditUser } from "@mocks/apps/privileges/payrollsForm.mock";
+import { projectsFormEditUser } from "@mocks/apps/privileges/projectsForm.mock";
 import { userEntriesDataMock } from "@mocks/apps/privileges/users.mock";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
+import { editUserTabsConfig } from "./config/editUserTabs.config";
+import { EditUserUI } from "./interface";
 
 function EditUser() {
   const { user_id } = useParams();
@@ -10,6 +15,15 @@ function EditUser() {
   const [selectedTab, setSelectedTab] = useState(
     editUserTabsConfig.generalInformation.id
   );
+
+  const [editData, setEditData] = useState({
+    generalInformation: { entries: {} },
+    branches: { entries: branchesFormEditUser },
+    projects: { entries: projectsFormEditUser },
+    events: { entries: eventsFormEditUser },
+    aidBudgetUnits: { entries: aidBudgetsFormEditUser },
+    payrolls: { entries: payrollsFormEditUser },
+  });
 
   const handleTabChange = (tabId) => {
     setSelectedTab(tabId);
@@ -20,6 +34,17 @@ function EditUser() {
     return userEntriesDataMock.find((user) => user.id === userID);
   }
 
+  const handleSubmit = (values) => {
+    const editKey = Object.keys(editUserTabsConfig).find(
+      (key) => editUserTabsConfig[key].id === selectedTab
+    );
+
+    setEditData((prevEditData) => ({
+      ...prevEditData,
+      [editKey]: { entries: values },
+    }));
+  };
+
   const user = getUserInformation();
 
   return (
@@ -27,6 +52,8 @@ function EditUser() {
       selectedTab={selectedTab}
       handleTabChange={handleTabChange}
       user={user}
+      editData={editData}
+      handleSubmit={handleSubmit}
     />
   );
 }
