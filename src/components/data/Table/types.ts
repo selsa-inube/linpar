f91d3106ae4b793237b21c45f61aa9c2e9713127
Juan Sebastian Lara <jsebastianlara3245@gmail.com>
2023-06-16 08:28:15 -0500

    refactor(table): change content parameter type

--- src/components/data/Table/types.ts
+++ src/components/data/Table/types.ts
@@ -1,7 +1,9 @@
 interface IAction {
   id: number;
   actionName: string;
-  content: (entry: Record<string, any>) => JSX.Element | JSX.Element;
+  content: (
+    entry: Record<string, string | number>
+  ) => JSX.Element | JSX.Element;
   type: string;
 }
 
