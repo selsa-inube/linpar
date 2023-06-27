02d0537d9677a601b47450213211dc6bf4d6929b
Juan Sebastian Lara <jsebastianlara3245@gmail.com>
2023-06-27 16:13:30 -0500

    fix(tables): change breakpoints

--- src/pages/privileges/outlets/users/config/usersTable.config.jsx
+++ src/pages/privileges/outlets/users/config/usersTable.config.jsx
@@ -1,7 +1,7 @@
 const usersBreakPointsConfig = [
   { breakpoint: "(min-width: 1091px)", totalColumns: 4 },
   { breakpoint: "(max-width: 1090px)", totalColumns: 3 },
-  { breakpoint: "(max-width: 980px)", totalColumns: 2 },
+  { breakpoint: "(max-width: 1002px)", totalColumns: 2 },
   { breakpoint: "(max-width: 850px)", totalColumns: 4 },
   { breakpoint: "(max-width: 680px)", totalColumns: 3 },
   { breakpoint: "(max-width: 550px)", totalColumns: 2 },
