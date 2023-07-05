4c40b726f2149ad2e13e5e62e5e2957ec1f852c9
Juan Sebastian Lara <jsebastianlara3245@gmail.com>
2023-07-05 16:03:25 -0500

    fix(fieldset): change padding top

--- src/components/inputs/Fieldset/styles.ts
+++ src/components/inputs/Fieldset/styles.ts
@@ -5,7 +5,7 @@ const StyledFieldset = styled.fieldset`
   border-radius: 15px;
   border-style: solid;
   border-color: ${colors.ref.palette.neutral.n40};
-  padding: 10px 24px 24px 24px;
+  padding: 14px 24px 24px 24px;
 `;
 
 export { StyledFieldset };
