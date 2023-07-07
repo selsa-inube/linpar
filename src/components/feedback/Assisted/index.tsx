6d57b6dfd6eff849dd450c986c531b77cac51182
Johan Daniel Garcia Nova <gtahghfsg@gmail.com>
2023-07-07 16:13:12 -0500

    refactor: removed conversion

--- src/components/feedback/Assisted/index.tsx
+++ src/components/feedback/Assisted/index.tsx
@@ -11,22 +11,18 @@ function Assisted(props: AssistedProps) {
   const { steps, currentStep, handleStepChange } = props;
 
   const handleNextStep = () => {
-    const currentStepIndex = steps.findIndex(
-      (step) => Number(step.id) === currentStep
-    );
+    const currentStepIndex = steps.findIndex((step) => step.id === currentStep);
     if (currentStepIndex === steps.length - 1) return;
     handleStepChange(steps[currentStepIndex + 1].id);
   };
 
   const handlePreviousStep = () => {
-    const currentStepIndex = steps.findIndex(
-      (step) => Number(step.id) === currentStep
-    );
+    const currentStepIndex = steps.findIndex((step) => step.id === currentStep);
     if (currentStepIndex === 0) return;
     handleStepChange(steps[currentStepIndex - 1].id);
   };
 
-  const currentStepInfo = steps.find((step) => Number(step.id) === currentStep);
+  const currentStepInfo = steps.find((step) => step.id === currentStep);
 
   return (
     <AssistedUI
