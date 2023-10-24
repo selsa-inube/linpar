03d9aadb744aeb555b4c0e80f7f6f437131882dd
cmarin001 <cmarin6051@gmail.com>
2023-10-24 09:55:43 -0500

    fix(routes): update commented-routes

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,26 +1,26 @@
-import { Routes } from "react-router-dom";
-// import { CheckingCredentials } from "@pages/login/outlets/CheckingCredentials";
-// import { Clients } from "@pages/login/outlets/Clients";
-// import { LoadingApp } from "@pages/login/outlets/LoadingApp";
-// import { ErrorPage } from "@components/layout/ErrorPage";
-// import { ErrorNotAvailable } from "@pages/login/errors/ErrorNotAvailable";
-// import { ErrorNotClient } from "@pages/login/errors/ErrorNotClient";
-// import { Login } from "@pages/login";
+import { Route, Routes } from "react-router-dom";
+import { CheckingCredentials } from "@pages/login/outlets/CheckingCredentials";
+import { Clients } from "@pages/login/outlets/Clients";
+import { LoadingApp } from "@pages/login/outlets/LoadingApp";
+import { ErrorPage } from "@components/layout/ErrorPage";
+import { ErrorNotAvailable } from "@pages/login/errors/ErrorNotAvailable";
+import { ErrorNotClient } from "@pages/login/errors/ErrorNotClient";
+import { Login } from "@pages/login";
 
 function LoginRoutes() {
   return (
     <Routes>
-      {/* <Route path="/" element={<Login />}>
-        <Route
+      {/* <Route path="/" element={<Login />}> */}
+      {/* <Route
           path="/:user_id/checking-credentials"
           element={<CheckingCredentials />}
-        />
-        <Route path="/:user_id/clients" element={<Clients />} />
-        <Route path="loading-app" element={<LoadingApp />} />
-      </Route>
-      <Route path="error/not-available" element={<ErrorNotAvailable />} />
-      <Route path="error/not-related-clients" element={<ErrorNotClient />} />
-      <Route path="/*" element={<ErrorPage />} /> */}
+        /> */}
+      {/* <Route path="/:user_id/clients" element={<Clients />} /> */}
+      {/* <Route path="loading-app" element={<LoadingApp />} /> */}
+      {/* </Route> */}
+      {/* <Route path="error/not-available" element={<ErrorNotAvailable />} /> */}
+      {/* <Route path="error/not-related-clients" element={<ErrorNotClient />} /> */}
+      {/* <Route path="/*" element={<ErrorPage />} /> */}
     </Routes>
   );
 }
--- src/routes/privileges.tsx
+++ src/routes/privileges.tsx
@@ -1,26 +1,26 @@
-import { Routes } from "react-router-dom";
-// import { PrivilegesOptions } from "@pages/privileges/outlets/options";
-// import { Invite } from "@pages/privileges/outlets/users/invite";
-// import { EditUser } from "@pages/privileges/outlets/users/edit-user";
-// import { Users } from "@pages/privileges/outlets/users";
-// import { Privileges } from "@pages/privileges";
-// import { ErrorPage } from "@components/layout/ErrorPage";
-// import { CompleteInvitation } from "@pages/privileges/outlets/users/complete-invitation";
+import { Route, Routes } from "react-router-dom";
+import { PrivilegesOptions } from "@pages/privileges/outlets/options";
+import { Invite } from "@pages/privileges/outlets/users/invite";
+import { EditUser } from "@pages/privileges/outlets/users/edit-user";
+import { Users } from "@pages/privileges/outlets/users";
+import { Privileges } from "@pages/privileges";
+import { ErrorPage } from "@components/layout/ErrorPage";
+import { CompleteInvitation } from "@pages/privileges/outlets/users/complete-invitation";
 
 function PrivilegesRoutes() {
   return (
     <Routes>
-      {/* <Route path="/" element={<Privileges />}>
-        <Route path="options" element={<PrivilegesOptions />} />
-        <Route path="users" element={<Users />} />
-        <Route path="users/invite" element={<Invite />} />
-        <Route path="users/edit/:user_id" element={<EditUser />} />
-        <Route
+      {/* <Route path="/" element={<Privileges />}> */}
+      {/* <Route path="options" element={<PrivilegesOptions />} /> */}
+      {/* <Route path="users" element={<Users />} /> */}
+      {/* <Route path="users/invite" element={<Invite />} /> */}
+      {/* <Route path="users/edit/:user_id" element={<EditUser />} /> */}
+      {/* <Route
           path="users/complete-invitation/:invitation_id"
           element={<CompleteInvitation />}
-        />
-      </Route>
-      <Route path="/*" element={<ErrorPage />} /> */}
+        /> */}
+      {/* </Route> */}
+      {/* <Route path="/*" element={<ErrorPage />} /> */}
     </Routes>
   );
 }
