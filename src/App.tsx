76ea7f7dc69e1c03de6b13eafb95960f472ab79b
nicolas <nicolasms0113@gmail.com>
2023-10-24 09:27:01 -0500

    chore(designSystem): update design-system to its version 5.8.0

--- src/App.tsx
+++ src/App.tsx
@@ -17,12 +17,12 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path="/" element={<Home />} errorElement={<ErrorPage />} />
-      <Route path="privileges/*" element={<PrivilegesRoutes />} />
+      {/* <Route path="privileges/*" element={<PrivilegesRoutes />} />
       <Route path="login/*" element={<LoginRoutes />} />
       <Route
         path="respond-invitation/:client_id/:invitation_id/*"
         element={<RespondInvitationRoutes />}
-      />
+      /> */}
     </>
   )
 );
