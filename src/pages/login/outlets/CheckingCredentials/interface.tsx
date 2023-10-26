445a866c6be80e3d3494ece66f1aeb481117676a
Andres Babativa <carlos3k11@gmail.com>
2023-10-26 11:24:35 -0500

    feat(CheckingCredentialsUI): update the inube props to current names

--- src/pages/login/outlets/CheckingCredentials/interface.tsx
+++ src/pages/login/outlets/CheckingCredentials/interface.tsx
@@ -4,10 +4,10 @@ function CheckingCredentialsUI() {
   return (
     <Stack gap="16px" direction="column">
       <Stack direction="column">
-        <Text type="title" size="large" align="center">
+        <Text type="title" size="large" textAlign="center">
           Validando credenciales
         </Text>
-        <Text type="title" size="small" align="center">
+        <Text type="title" size="small" textAlign="center">
           Espere un momento, por favor.
         </Text>
       </Stack>
