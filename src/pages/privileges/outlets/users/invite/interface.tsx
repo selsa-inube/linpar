6b1e33b2dd734918f8835e6c703028e073cf1ca8
cmarin001 <cmarin6051@gmail.com>
2023-11-07 16:03:00 -0500

    refactor(inviteUI): use new tokens

--- src/pages/privileges/outlets/users/invite/interface.tsx
+++ src/pages/privileges/outlets/users/invite/interface.tsx
@@ -5,12 +5,14 @@ import {
   Stack,
   Textfield,
   SectionMessage,
+  useMediaQueries,
+  Grid,
 } from "@inube/design-system";
 import { MdOutlineShortcut } from "react-icons/md";
 import { messageInvitationSentConfig } from "./config/messageInvitationSent.config";
 import { usersInvitationsConfig } from "./config/usersInvitations.config";
 import { EMessageType } from "@src/types/messages.types";
-import { StyledFormContainer, StyledPageUsers } from "./styles";
+import { StyledPageUsers } from "./styles";
 import { FormikValues } from "formik";
 
 interface InviteUIProps {
@@ -68,6 +70,9 @@ function InviteUI(props: InviteUIProps) {
     handleSubmit,
   } = props;
 
+  const mediaQueries = ["(max-width: 1111px)", "(max-width: 565px)"];
+  const matches = useMediaQueries(mediaQueries);
+
   return (
     <StyledPageUsers>
       <Stack gap="48px" direction="column">
@@ -81,7 +86,15 @@ function InviteUI(props: InviteUIProps) {
         </Stack>
         <form>
           <Stack gap="32px" alignItems="flex-end" direction="column">
-            <StyledFormContainer>
+            <Grid
+              templateColumns={
+                matches["(max-width: 1111px)"] ? "1fr" : "repeat(2, 1fr)"
+              }
+              gap={"s300"}
+              margin={"s0 s0 s400 s0"}
+              width={"100%"}
+              autoRows="unset"
+            >
               <Textfield
                 label="Nombre"
                 placeholder="Ingresa su nombre completo"
@@ -157,7 +170,7 @@ function InviteUI(props: InviteUIProps) {
                 onChange={formik.handleChange}
                 onBlur={formik.handleBlur}
               />
-            </StyledFormContainer>
+            </Grid>
             <Button
               type="button"
               appearance="success"
--- src/pages/privileges/outlets/users/invite/styles.ts
+++ src/pages/privileges/outlets/users/invite/styles.ts
@@ -1,22 +1,13 @@
 import styled from "styled-components";
+import { inube } from "@inube/design-system";
 
 const StyledPageUsers = styled.div`
-  padding: 32px 64px;
+  padding: ${inube.spacing.s400} ${inube.spacing.s800};
 
   @media (max-width: 565px) {
-    padding: 16px 16px 100px 16px;
+    padding: ${inube.spacing.s200} ${inube.spacing.s200}
+      calc(${inube.spacing.s250}*5) ${inube.spacing.s200};
   }
 `;
 
-const StyledFormContainer = styled.div`
-  width: 100%;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  gap: 24px;
-
-  @media (max-width: 1111px) {
-    grid-template-columns: repeat(1, 1fr);
-  }
-`;
-
-export { StyledPageUsers, StyledFormContainer };
+export { StyledPageUsers };
