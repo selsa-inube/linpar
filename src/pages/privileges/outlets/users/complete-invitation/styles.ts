fe6390a5b6671b5fd1fb7052eba9441b29f9e9c6
Andres Babativa <carlos3k11@gmail.com>
2023-11-07 15:09:37 -0500

    style(CompleteInvitationUI): implement spacing tokens

--- src/pages/privileges/outlets/users/complete-invitation/styles.ts
+++ src/pages/privileges/outlets/users/complete-invitation/styles.ts
@@ -1,11 +1,15 @@
 import styled from "styled-components";
+import { inube } from "@inube/design-system";
 
 interface IStyledContainer {
   smallScreen: boolean;
 }
 
 const StyledContainer = styled.div<IStyledContainer>`
-  padding: ${(props) => (props.smallScreen ? "16px" : "32px 64px")};
+  padding: ${({ smallScreen }) =>
+    smallScreen
+      ? inube.spacing.s200
+      : `${inube.spacing.s400} ${inube.spacing.s800}`};
 `;
 
 export { StyledContainer };
