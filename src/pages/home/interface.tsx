89581f3bb2b2ae1e3b4a5611c5fb18310afed57a
cmarin001 <cmarin6051@gmail.com>
2023-11-21 17:43:32 -0500

    refactor(homeUI): remove the former use StyledAppsList and StyledPageTitle

--- src/pages/home/interface.tsx
+++ src/pages/home/interface.tsx
@@ -1,11 +1,15 @@
 import { useContext } from "react";
 import { MdOutlineDoorFront } from "react-icons/md";
-import { Header } from "@inube/design-system";
+import { Header, Grid, Stack, useMediaQueries } from "@inube/design-system";
 
 import { AppContext } from "@src/context";
 import { PageTitle } from "@components/PageTitle";
 import { AppCard } from "@components/cards/AppCard";
-
+import {
+  mediaQuerySettings,
+  appListpaddingSettings,
+  pageTitlePaddingSettings,
+} from "./types";
 import {
   StyledContentImg,
   StyledLogo,
@@ -13,7 +17,41 @@ import {
 
 import { IApps } from "./types";
 import { navigationConfig } from "./config/apps.config";
-import { StyledAppsList, StyledHome, StyledPageTitle } from "./styles";
+import { StyledAppsList, StyledHome } from "./styles";
+
+const getTemplateColumns = (
+  mediaQueries: any[],
+  matches: { [x: string]: unknown },
+  templateColumnsSettings: { [x: string]: string }
+): string => {
+  const matchingQuery = mediaQueries.find(
+    (mediaQuerie) => matches[mediaQuerie]
+  );
+  return matchingQuery
+    ? templateColumnsSettings[matchingQuery]
+    : "repeat(5, 250px)";
+};
+
+const getPadding = (
+  mediaQueries: any[],
+  matches: { [x: string]: unknown },
+  appListpaddingSettings: { [x: string]: string }
+): string => {
+  const matchingQuery = mediaQueries.find(
+    (mediaQuerie) => matches[mediaQuerie]
+  );
+  return matchingQuery ? appListpaddingSettings[matchingQuery] : "s0 s800";
+};
+
+const getPageTitlePadding = (
+  matches: { [x: string]: unknown },
+  pageTitlePaddingSettings: { [x: string]: string }
+) => {
+  const matchingQuery = Object.keys(pageTitlePaddingSettings).find(
+    (mediaQuerie) => matches[mediaQuerie]
+  );
+  return matchingQuery ? pageTitlePaddingSettings[matchingQuery] : "s400 s800";
+};
 
 const renderLogo = (imgUrl: string) => {
   return (
@@ -31,6 +69,12 @@ function HomeUI(props: HomeUIProps) {
   const { apps } = props;
   const { user } = useContext(AppContext);
 
+  const mediaQueries = Object.keys(
+    mediaQuerySettings
+  ) as (keyof typeof mediaQuerySettings)[];
+
+  const matches = useMediaQueries(mediaQueries);
+
   return (
     <StyledHome>
       <Header
@@ -40,25 +84,41 @@ function HomeUI(props: HomeUIProps) {
         userName={user.username}
         client={user.company}
       />
-      <StyledPageTitle>
+      <Stack
+        id="PageTitle"
+        gap="4px"
+        padding={getPageTitlePadding(matches, pageTitlePaddingSettings)}
+      >
         <PageTitle
           title={`Bienvenido ${user.username}`}
           description="Selecciona una opción para empezar a ajustar la configuración de tu software Linix"
           icon={<MdOutlineDoorFront />}
         />
-      </StyledPageTitle>
+      </Stack>
       <StyledAppsList>
-        {apps.map((app) => (
-          <li key={app.url}>
-            <AppCard
-              key={app.id}
-              label={app.label}
-              description={app.description}
-              icon={app.icon}
-              url={app.url}
-            />
-          </li>
-        ))}
+        <Grid
+          templateColumns={getTemplateColumns(
+            mediaQueries,
+            matches,
+            mediaQuerySettings
+          )}
+          justifyContent="center"
+          gap="s300"
+          padding={getPadding(mediaQueries, matches, appListpaddingSettings)}
+          margin="s0 auto"
+        >
+          {apps.map((app) => (
+            <li key={app.url}>
+              <AppCard
+                key={app.id}
+                label={app.label}
+                description={app.description}
+                icon={app.icon}
+                url={app.url}
+              />
+            </li>
+          ))}
+        </Grid>
       </StyledAppsList>
     </StyledHome>
   );
--- src/pages/home/types.ts
+++ src/pages/home/types.ts
@@ -6,4 +6,22 @@ interface IApps {
   url: string;
 }
 
+const mediaQuerySettings: Record<string, string> = {
+  "(max-width: 590px)": "repeat(1, 100%)",
+  "(max-width: 660px)": "repeat(1, 100%)",
+  "(max-width: 930px)": "repeat(2, 250px)",
+  "(max-width: 1210px)": "repeat(3, 250px)",
+  "(max-width: 1480px)": "repeat(4, 250px)",
+};
+
+const appListpaddingSettings: Record<string, string> = {
+  "(max-width: 590px)": "s0 s200",
+  "(max-width: 660px)": "s0 s400",
+};
+
+const pageTitlePaddingSettings = {
+  "(max-width: 590px)": "s300 s200 s400",
+  "(max-width: 660px)": "s300 s400 s400",
+};
+export { mediaQuerySettings, appListpaddingSettings, pageTitlePaddingSettings };
 export type { IApps };
