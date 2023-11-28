d69ebdce82b06e58a380e64201ccb6658829ea14
cmarin001 <cmarin6051@gmail.com>
2023-11-28 09:41:46 -0500

    fix(subjectCard): remove unnecessary code

--- src/components/cards/SubjectCard/styles.ts
+++ src/components/cards/SubjectCard/styles.ts
@@ -12,17 +12,17 @@ const StyledSubjectCard = styled.div<IStyledSubjectCard>`
     box-sizing: border-box;
     border-radius: ${inube.spacing.s100};
     border: 1px solid
-      ${({ theme }: IStyledSubjectCard) =>
+      ${({ theme }) =>
         theme?.color?.surface?.gray?.regular ||
         inube.color.surface.gray.regular};
     cursor: pointer;
-    background-color: ${({ theme, isActive }: IStyledSubjectCard) =>
+    background-color: ${({ theme, isActive }) =>
       isActive
         ? theme?.color?.surface?.gray?.regular ||
           inube.color.surface.gray.regular
         : theme?.color?.surface?.light?.clear ||
           inube.color.surface.light.clear};
-    box-shadow: ${({ isActive }: IStyledSubjectCard) =>
+    box-shadow: ${({ isActive }) =>
       isActive ? "none" : "0px 1px 2px " + inube.color.surface.gray.regular};
     & div:first-child {
       display: ${(props) => (props.smallScreen ? "none" : "block")};
