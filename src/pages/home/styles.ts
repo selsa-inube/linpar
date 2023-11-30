39bf031a15bbe8f74e2f3215dd88a09b50f672f2
cmarin001 <cmarin6051@gmail.com>
2023-11-29 19:08:32 -0500

    fix(appMenu): adjust the layout in order to allow a scroll when there is a small screen

--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -4,10 +4,6 @@ const StyledHome = styled.div`
   box-sizing: border-box;
   padding-bottom: 40px;
   height: 100vh;
-  overflow-y: scroll;
-  #PageTitle {
-    max-width: 1400px;
-  }
   li {
     list-style: none;
   }
