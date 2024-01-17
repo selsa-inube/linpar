3e6d9199fb46052ccdb7c7fee63286f42fa7f8a7
carin <ca.rincon97@gmail.com>
2024-01-17 08:39:36 -0500

    refact: only 1 public component

--- src/components/feedback/SendingInformation/index.tsx
+++ src/components/feedback/SendingInformation/index.tsx
@@ -1,9 +1,97 @@
 import { useState } from "react";
-import { Button, Stack, inube } from "@inube/design-system";
+import {
+  Button,
+  Stack,
+  inube,
+  useMediaQuery,
+  Text,
+  CountdownBar,
+  Icon,
+} from "@inube/design-system";
 import { sectionMessageConfig } from "./props";
 import { sectionMessageState } from "./config/sectionMessage.state";
 import { IoMdInformationCircleOutline } from "react-icons/io";
-import { SectionMessageCustomized } from "./sectionMessagesCustomized";
+import { MdClear } from "react-icons/md";
+import { StyledSectionMessage } from "./styles";
+import { Appearance } from "./types";
+
+export interface ISectionMessageProps {
+  children?: React.ReactNode;
+  icon: JSX.Element;
+  title: string;
+  description: string;
+  appearance: Appearance;
+  duration: number;
+  closeSectionMessage: () => void;
+  isMessageResponsive: boolean;
+}
+
+const SectionMessageCustomized = (props: ISectionMessageProps) => {
+  const {
+    children,
+    icon,
+    title,
+    description,
+    appearance = "primary",
+    duration,
+    closeSectionMessage,
+  } = props;
+
+  const [isPaused, setIsPaused] = useState(false);
+  const isMessageResponsive = useMediaQuery("(max-width: 565px)");
+
+  const newDescription = description.substring(0, 240);
+
+  return (
+    <StyledSectionMessage
+      appearance={appearance}
+      onMouseEnter={() => setIsPaused(true)}
+      onMouseLeave={() => setIsPaused(false)}
+      isMessageResponsive={isMessageResponsive}
+    >
+      <Stack justifyContent="space-between" padding="s200">
+        <Stack
+          gap="16px"
+          alignItems={isMessageResponsive ? "center" : undefined}
+        >
+          <Stack alignItems="center" gap="16px">
+            <Icon
+              size="24px"
+              spacing="wide"
+              appearance={appearance}
+              icon={icon}
+            />
+            <Stack direction="column" gap="6px">
+              <Text size="large">{title}</Text>
+              {!isMessageResponsive && (
+                <Text size="small" appearance="gray">
+                  {newDescription}
+                </Text>
+              )}
+              {children}
+            </Stack>
+          </Stack>
+        </Stack>
+        <Stack alignItems={isMessageResponsive ? "center" : undefined}>
+          <Icon
+            size="16px"
+            onClick={closeSectionMessage}
+            appearance={appearance}
+            icon={<MdClear />}
+          />
+        </Stack>
+      </Stack>
+      {duration && (
+        <CountdownBar
+          paused={isPaused}
+          appearance={appearance}
+          duration={duration}
+          onCountdown={closeSectionMessage}
+        />
+      )}
+    </StyledSectionMessage>
+  );
+};
 
 const handleCancel = () => {
   let newSectionMessageState = { ...sectionMessageState };
@@ -21,7 +109,7 @@ const handleAgree = () => {
   return;
 };
 
-const SendInformationMessage = () => {
+const SendInformationMessage = (appearance: Appearance) => {
   let [cancelDisable, setCancelDisable] = useState(true);
 
   return (
@@ -35,7 +123,7 @@ const SendInformationMessage = () => {
               duration={sectionMessageConfig.duration}
               closeSectionMessage={() => setCancelDisable(false)}
               icon={<IoMdInformationCircleOutline />}
-              appearance={"primary"}
+              appearance={appearance}
               isMessageResponsive={false}
             >
               <Stack width="100%" gap={inube.spacing.s050}>
