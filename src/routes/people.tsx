6bbc920b4c87d48344ff1815d016edd82694d70d
carin <ca.rincon97@gmail.com>
2024-01-17 02:06:53 -0500

    feat: new token management

--- src/routes/people.tsx
+++ src/routes/people.tsx
@@ -6,19 +6,52 @@ import { Palette } from "@src/pages/people/outlets/palette";
 import { Texts } from "@src/pages/people/outlets/texts";
 import { Surfaces } from "@src/pages/people/outlets/surfaces";
 import { Lines } from "@src/pages/people/outlets/lines";
+import { useState } from "react";
+import { inube, presente } from "@inube/design-system";
 
 function PeopleRoutes() {
+  const [token, setToken] = useState({ ...presente });
+
+  const handleTokenChange = (domain, block, tokenUpdate) => {
+    const updatedTokenColor = {
+      ...token.color,
+      [block]: { ...tokenUpdate },
+    };
+
+    const updatedToken = {
+      ...token,
+      [domain]: { updatedTokenColor },
+    };
+    setToken(updatedToken);
+  };
+
   return (
-    <Routes>
-      <Route path="/" element={<People />}>
-        <Route path="options" element={<PeopleOptions />} />
-        <Route path="palette" element={<Palette />} />
-        <Route path="texts" element={<Texts />} />
-        <Route path="surfaces" element={<Surfaces />} />
-        <Route path="lines" element={<Lines />} />
-      </Route>
-      <Route path="/*" element={<ErrorPage />} />
-    </Routes>
+    <>
+      {console.log(presente)}
+      {console.log(inube)}
+      <Routes>
+        <Route path="/" element={<People />}>
+          <Route path="options" element={<PeopleOptions />} />
+          <Route
+            path="palette"
+            element={<Palette token={token} setToken={handleTokenChange} />}
+          />
+          <Route
+            path="texts"
+            element={<Texts token={token} setToken={handleTokenChange} />}
+          />
+          <Route
+            path="surfaces"
+            element={<Surfaces token={token} setToken={handleTokenChange} />}
+          />
+          <Route
+            path="lines"
+            element={<Lines token={token} setToken={handleTokenChange} />}
+          />
+        </Route>
+        <Route path="/*" element={<ErrorPage />} />
+      </Routes>
+    </>
   );
 }
 
