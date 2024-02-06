0617fdff68675605784d90787ef8df48fa980550
lincon ossa <ossalincon422@gmail.com>
2024-02-06 11:21:59 -0500

    fix: folder that was created is deleted

--- .vite/deps_temp_2e6d6ae9/react-dom.js
+++
@@ -1,5 +0,0 @@
-import { require_react_dom } from "./chunk-D5SJRT53.js";
-import "./chunk-ZAUFE7H7.js";
-import "./chunk-UXIASGQL.js";
-export default require_react_dom();
-//# sourceMappingURL=react-dom.js.map
