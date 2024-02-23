2962bdce311d34796463b57c71e1b3228caa485a
Andres Babativa <carlos3k11@gmail.com>
2024-02-23 16:41:38 -0500

    feat: add client-server mock data initialization

--- src/mocks/utils/initializeDataDB.ts
+++ src/mocks/utils/initializeDataDB.ts
@@ -2,6 +2,7 @@ import localforage from "localforage";
 import { intializedData } from "@mocks/utils/dataMuck.service";
 import { intializedTokenData } from "@mocks/themeService/themeService.mock";
 import { DocumentsServiceMock } from "@mocks/privileges/documents/DocumentsServiceMock.mock";
+import { clientServerMock } from "../privileges/client-server/client-serverServiceMock.mock";
 
 export function initializeDataDB() {
   localforage.clear();
@@ -10,5 +11,10 @@ export function initializeDataDB() {
   intializedData<typeof DocumentsServiceMock[number]>(
     "documents",
     DocumentsServiceMock
+  );
+
+  intializedData<typeof clientServerMock[number]>(
+    "clients-server",
+    clientServerMock
   );
 }
