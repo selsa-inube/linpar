9d729eab45ca13fa739c0618a5dea0077ea68149
carin <ca.rincon97@gmail.com>
2024-02-26 21:40:43 -0500

    fix(AppContext): decoupling types

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,10 @@ import { Auth0Provider } from "@auth0/auth0-react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+//q: how can i redirects auth0 to localohost:3000 during development and to the production url during production?
+//a: you can use the window.location.origin to get the current url and use it as the redirect_uri
+//q: what to do if the use of window.location.origin is not working?
+//a: you can use the import.meta.env.VITE_ prefix to access environment variables
 
 const AUTH0_DOMAIN = import.meta.env.VITE_AUTH0_DOMAIN;
 const AUTH0_CLIENT_ID = import.meta.env.VITE_AUTH0_CLIENT_ID;
