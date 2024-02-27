0535e8abfe5fd0852eea6226506537438fba8f37
Andres Babativa <carlos3k11@gmail.com>
2024-02-27 12:03:08 -0500

    docs(Accordion.controller): add ControllerAccordion component to Accordion stories

---
+++ src/components/data/Accordion/stories/ControllerAccordion.tsx
@@ -0,0 +1,53 @@
+import { MdArrowBack } from "react-icons/md";
+import { Button, Grid, Stack, useMediaQuery } from "@inube/design-system";
+
+import { BoxAttribute } from "../../BoxAttirbute";
+import { MockDataSummaryPage } from "./Accordion.stories";
+import { Accordion } from "..";
+
+export interface IControllerAccordionProps {
+  steps: MockDataSummaryPage;
+}
+
+export const ControllerAccordion = (props: IControllerAccordionProps) => {
+  const { steps } = props;
+
+  const isMobile = useMediaQuery("(max-width: 740px)");
+
+  return (
+    <Stack gap="8px" direction="column">
+      {steps.map((step) => {
+        const { section, attributes } = step;
+        return (
+          <Accordion key={section} title={section}>
+            <Grid
+              templateColumns={isMobile ? "1fr" : "repeat(2, 1fr)"}
+              width="-webkit-fill-available"
+              autoRows="auto"
+            >
+              {attributes.map((attribute) => (
+                <BoxAttribute
+                  key={attribute.attribute}
+                  attribute={attribute.attribute}
+                  value={attribute.value}
+                />
+              ))}
+            </Grid>
+            <Stack justifyContent="flex-end" width="100%">
+              <Button
+                variant="none"
+                appearance="dark"
+                spacing="compact"
+                iconBefore={<MdArrowBack />}
+                type="link"
+                path="/"
+              >
+                Regresar a este paso
+              </Button>
+            </Stack>
+          </Accordion>
+        );
+      })}
+    </Stack>
+  );
+};
