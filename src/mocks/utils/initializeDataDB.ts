b4152be0fa0bdd3ccd304a7e5493d65abadb89fa
lincon ossa <ossalincon422@gmail.com>
2024-02-27 17:38:38 -0500

    feat: creating new files

--- src/mocks/utils/initializeDataDB.ts
+++ src/mocks/utils/initializeDataDB.ts
@@ -3,6 +3,7 @@ import { intializedData } from "@mocks/utils/dataMuck.service";
 import { intializedTokenData } from "@mocks/themeService/themeService.mock";
 import { DocumentsServiceMock } from "@mocks/privileges/documents/DocumentsServiceMock.mock";
 import { clientServerMock } from "@mocks/privileges/client-server/client-serverServiceMock.mock";
+import { optionsWebMock } from "@mocks/privileges/options/optionsWebMock.mock";
 
 export function initializeDataDB() {
   localforage.clear();
@@ -17,4 +18,6 @@ export function initializeDataDB() {
     "clients-server",
     clientServerMock
   );
+
+  intializedData<typeof optionsWebMock[number]>("options-web", optionsWebMock);
 }
