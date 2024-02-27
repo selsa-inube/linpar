15168bcb7c78111e24c2161b2d400de13b552d30
Andres Babativa <carlos3k11@gmail.com>
2024-02-26 20:53:53 -0500

    feat(Accordion.stories): add decorators and update Default story in Accordion component

--- src/components/data/Accordion/Accordion.stories.tsx
+++ src/components/data/Accordion/Accordion.stories.tsx
@@ -1,12 +1,33 @@
 //import { BoxAttribute } from "@components/cards/BoxAttribute";
+import { BrowserRouter } from "react-router-dom";
+import { StoryFn } from "@storybook/react";
+import { MdArrowBack } from "react-icons/md";
+import { Button, Stack } from "@inube/design-system";
+import { BoxAttribute } from "../BoxAttirbute";
 import { Accordion, IAccordionProps } from ".";
 
 const story = {
   title: "data/Accordion",
   component: Accordion,
+  decorators: [
+    (Story: StoryFn) => (
+      <BrowserRouter>
+        <Story />
+      </BrowserRouter>
+    ),
+  ],
 };
 
-export const Default = (args: IAccordionProps) => <Accordion {...args} />;
+export const Default = (args: IAccordionProps) => (
+  <Accordion {...args}>
+    <BoxAttribute attribute="Attribute" value="Value" />
+    <Stack justifyContent="flex-end" width="100%">
+      <Button appearance="dark" variant="none" iconBefore={<MdArrowBack />}>
+        Regresar a este paso
+      </Button>
+    </Stack>
+  </Accordion>
+);
 Default.args = {
   title: "Title",
   children: "Content",
