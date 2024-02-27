d750dea4a8d36588c7aa8e3ab6f91ece31aa2fc8
Andres Babativa <carlos3k11@gmail.com>
2024-02-27 12:01:24 -0500

    style(BoxAttribute): remove width property

--- src/components/data/BoxAttirbute/styles.ts
+++ src/components/data/BoxAttirbute/styles.ts
@@ -3,7 +3,6 @@ import { inube } from "@inube/design-system";
 
 export const StyledBoxAttribute = styled.div`
   display: flex;
-  width: -webkit-fill-available;
   height: 32px;
   flex-direction: row;
   justify-content: space-between;
