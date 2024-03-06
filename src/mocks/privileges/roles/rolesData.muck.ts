e36ef6b2fc9981cde7ac35b63ccdcfc11c9345fe
Andres Babativa <carlos3k11@gmail.com>
2024-03-06 15:47:32 -0500

    refactor: update rolesData.muck.ts with new application roles

--- src/mocks/privileges/roles/rolesData.muck.ts
+++ src/mocks/privileges/roles/rolesData.muck.ts
@@ -3,24 +3,24 @@ export const roles = [
     id: "11",
     code: 1121212,
     nameRoles: "Incluir las opciones web 30,31 y 32",
-    aplicaicionRoles: "Insertar",
+    aplicaicionRoles: "cooservunal",
   },
   {
     id: "12",
     code: 23123213,
     nameRoles: "Incluir la opción botón CON_ASIGNA_PRIV",
-    aplicaicionRoles: "Insertar",
+    aplicaicionRoles: "corbanca",
   },
   {
     id: "13",
     code: 3123213,
     nameRoles: "Quitar la opción botón USUA_COPIAR_PRIV",
-    aplicaicionRoles: "Modificar",
+    aplicaicionRoles: "fedecom",
   },
   {
     id: "14",
     code: 1233123,
     nameRoles: "Quitar la opción ciente-servidor GCATIPCONT_0",
-    aplicaicionRoles: "Eliminar",
+    aplicaicionRoles: "presente",
   },
 ];
