f83bb5f31b712b1f5194ef5190611fa7eb3ece24
lincon ossa <ossalincon422@gmail.com>
2024-03-20 20:36:45 -0500

    fix: correction documents

--- src/components/AssistedFinalStep/index.tsx
+++ src/components/AssistedFinalStep/index.tsx
@@ -14,14 +14,17 @@ export const ControllerAccordion = (props: IControllerAccordionProps) => {
   const isMobile = useMediaQuery("(max-width: 740px)");
   const generalInformation = formData.generalInformation.values;
   const clientServerButton = formData.clientServerButton.values;
+  const downloadableDocuments = formData.downloadableDocuments.values;
+  const webReports = formData.webReports.values;
+  const webOptions = formData.webOptions.values;
 
-  const webOptions = formData.webOptions.values.filter(
-    (value) => value.isActive
-  );
   const clientServerOptions = formData.clientServerOptions.values.filter(
     (value) => value.isActive
   );
 
+  const selectedWebOption = formData.webOptions.values.filter(
+    (option) => option.isActive
+  );
   return (
     <Stack gap="8px" direction="column">
       <>
@@ -48,10 +51,16 @@ export const ControllerAccordion = (props: IControllerAccordionProps) => {
               attribute="Acción caso de uso:"
               value={generalInformation.i_Tipusec}
             />
-            <BoxAttribute
-              attribute="Opciones Web:"
-              value={webOptions.map((option) => option.value).join(", ")}
-            />
+            {webOptions
+              .filter((option) => option.isActive)
+              .slice(0, 1)
+              .map((option) => (
+                <BoxAttribute
+                  key={option.id}
+                  attribute="Opción Web:"
+                  value={option.value}
+                />
+              ))}
             <BoxAttribute
               attribute="Opción cliente servidor:"
               value={clientServerOptions
@@ -80,7 +89,7 @@ export const ControllerAccordion = (props: IControllerAccordionProps) => {
           defaultOpen={!isMobile}
         >
           <BoxAttribute
-            attribute="Opción cliente servidor:"
+            attribute="Selección cliente servidor:"
             value={clientServerButton.csButtonOption}
           />
           <Stack justifyContent="flex-end" width="100%">
@@ -96,6 +105,118 @@ export const ControllerAccordion = (props: IControllerAccordionProps) => {
             </Button>
           </Stack>
         </Accordion>
+      </>
+      <>
+        <Accordion
+          key={"Formatos descargables"}
+          title={"Formatos descargables"}
+          defaultOpen={!isMobile}
+        >
+          <Grid
+            templateColumns={isMobile ? "1fr" : "repeat(2, 1fr)"}
+            width="-webkit-fill-available"
+            autoRows="auto"
+            gap="s200"
+          >
+            <>
+              {downloadableDocuments
+                .filter((option) => option.isActive)
+                .map((option, index) => (
+                  <BoxAttribute
+                    key={index}
+                    attribute={`Formato descargable:`}
+                    value={option.value}
+                  />
+                ))}
+            </>
+          </Grid>
+          <Stack justifyContent="flex-end" width="100%">
+            <Button
+              variant="none"
+              appearance="dark"
+              spacing="compact"
+              iconBefore={<MdArrowBack />}
+              onClick={() => handleStepChange(2)}
+              type="link"
+            >
+              Regresar a este paso
+            </Button>
+          </Stack>
+        </Accordion>
+      </>
+      <>
+        <Accordion
+          key={"Reportes web"}
+          title={"Reportes web"}
+          defaultOpen={!isMobile}
+        >
+          <Grid
+            templateColumns={isMobile ? "1fr" : "repeat(2, 1fr)"}
+            width="-webkit-fill-available"
+            autoRows="auto"
+            gap="s200"
+          >
+            <>
+              {webReports
+                .filter((option) => option.isActive)
+                .map((option, index) => (
+                  <BoxAttribute
+                    key={index}
+                    attribute={`Reportes web:`}
+                    value={option.value}
+                  />
+                ))}
+            </>
+          </Grid>
+          <Stack justifyContent="flex-end" width="100%">
+            <Button
+              variant="none"
+              appearance="dark"
+              spacing="compact"
+              iconBefore={<MdArrowBack />}
+              onClick={() => handleStepChange(3)}
+              type="link"
+            >
+              Regresar a este paso
+            </Button>
+          </Stack>
+        </Accordion>
+        <>
+          <Accordion
+            key={"Opciones web"}
+            title={"Opciones web"}
+            defaultOpen={!isMobile}
+          >
+            <Grid
+              templateColumns={isMobile ? "1fr" : "repeat(2, 1fr)"}
+              width="-webkit-fill-available"
+              autoRows="auto"
+              gap="s200"
+            >
+              <>
+                {selectedWebOption.map((option) => (
+                  <BoxAttribute
+                    key={option.id}
+                    attribute={`Opción web:`}
+                    value={option.value}
+                  />
+                ))}
+              </>
+            </Grid>
+            <Stack justifyContent="flex-end" width="100%">
+              <Button
+                variant="none"
+                appearance="dark"
+                spacing="compact"
+                iconBefore={<MdArrowBack />}
+                onClick={() => handleStepChange(4)}
+                type="link"
+              >
+                Regresar a este paso
+              </Button>
+            </Stack>
+          </Accordion>
+        </>
       </>
     </Stack>
   );
