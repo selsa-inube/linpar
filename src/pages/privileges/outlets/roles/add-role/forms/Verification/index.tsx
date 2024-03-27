9c931ac9d578a72da97f6259806bb70e01a084d1
Andres Babativa <carlos3k11@gmail.com>
2024-03-27 12:44:07 -0500

    refactor(VerificationAddRole): remove unnecessary code

--- src/pages/privileges/outlets/roles/add-role/forms/Verification/index.tsx
+++ src/pages/privileges/outlets/roles/add-role/forms/Verification/index.tsx
@@ -43,73 +43,7 @@ export const VerificationAddRole = (props: IControllerAccordionProps) => {
   const { steps } = props;
 
   const isMobile = useMediaQuery("(max-width: 740px)");
-  /*
-  const dataVerificationStep = [steps].map((data) => ({
-    sections: {
-      generalInformation: {
-        title: "Información general",
-        attributes: [
-          {
-            attribute: "Nombre del rol",
-            value: data.generalInformation.values.roleName,
-          },
-          {
-            attribute: "Descripción",
-            value: data.generalInformation.values.description,
-          },
-          {
-            attribute: "Aplicación",
-            value: data.generalInformation.values.aplication,
-          },
-        ],
-      },
-      ancillaryAccounts: {
-        title: "Cuentas auxiliares",
-        attributes: [
-          {
-            attribute: "Sector oficial",
-            value: data.ancillaryAccounts.values.officialSector,
-          },
-          {
-            attribute: "Sector comercial",
-            value: data.ancillaryAccounts.values.commercialSector,
-          },
-          {
-            attribute: "Sector solidario",
-            value: data.ancillaryAccounts.values.solidaritySector,
-          },
-        ],
-      },
 
-      transactionTypes: {
-        title: "Tipos de transacción",
-        attributes: data.transactionTypes?.values
-          .filter(
-            (transactionType: IInitialiceFormRole) =>
-              transactionType.isActive === true
-          )
-          .map((transactionType: IInitialiceFormRole) => ({
-            attribute: transactionType.value,
-            value: transactionType.value,
-          })),
-      },
-      businessRules: {
-        title: "Reglas de negocio",
-        attributes: data?.businessRules?.values
-          .filter(
-            (businessRule: { isActive: boolean }) =>
-              businessRule.isActive === true
-          )
-          .map((businessRule: IInitialiceFormRole) => ({
-            attribute: businessRule.value,
-            value: businessRule.value,
-          })),
-      },
-    },
-  }));
- */
-
-  // Estructura de datos mejorada
   const dataVerificationStep: DataVerificationStep[] = [steps].map((data) => ({
     sections: {
       generalInformation: {
@@ -163,21 +97,9 @@ export const VerificationAddRole = (props: IControllerAccordionProps) => {
     },
   }));
 
-  /*  const sections = dataVerificationStep.flatMap((step) =>
-    Object.values(step.sections).map((section) => section.title)
-  ); */
-
-  /*  const attributes = dataVerificationStep.flatMap((step) =>
-    Object.values(step.sections).flatMap((attributes) => attributes.attributes)
-  ); */
-
   const keySections = dataVerificationStep.flatMap((step) =>
     Object.keys(step.sections)
   );
-
-  /*   console.log(keySections);
-
-  dataVerificationStep.map((step) => console.log(step.sections)); */
 
   return (
     <VerificationAddRoleUI
