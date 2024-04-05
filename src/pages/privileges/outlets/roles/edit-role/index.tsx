7c6c3005242bbaa7a32bc7b8f4c0c807f249ccc5
Andres Babativa <carlos3k11@gmail.com>
2024-04-04 23:59:04 -0500

    refactor: refactor EditRole component and EditRoleUI interface

--- src/pages/privileges/outlets/roles/edit-role/index.tsx
+++ src/pages/privileges/outlets/roles/edit-role/index.tsx
@@ -1,18 +1,95 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { EditRoleUI } from "./interface";
+import { useMediaQuery } from "@inube/design-system";
 
-export const pruebaData = (data: any) => {
-  return data;
-};
+import { getData } from "@src/mocks/utils/dataMock.service";
+
+import { stepsAddRol } from "../add-role/config/addRol.config";
+import { EditRoleUI } from "./interface";
+import { IRol } from "../types";
+
+const Tabs = Object.values(stepsAddRol)
+  .filter((item) => item.label !== "Verificación")
+  .map((stepAddRole) => ({
+    id: stepAddRole.label,
+    label: stepAddRole.label,
+    isDisabled: false,
+  }));
 
 export const EditRole = () => {
   const { rol_id } = useParams();
 
-  console.log(rol_id);
+  const [loading, setLoading] = useState(true);
+
+  const [selectedTab, setSelectedTab] = useState(
+    stepsAddRol.generalInformation.label
+  );
+
+  const smallScreen = useMediaQuery("(max-width: 580px)");
+
+  const [editData, setEditData] = useState<IRol>({
+    i_Activo: "Y",
+    k_Rol: "",
+    k_Tipcon: "",
+    n_Rol: "",
+    n_Uso: "",
+  });
+  /* 
+  useEffect(() => {
+    getData("linix-roles")
+      .then((data) => {
+        if (Array.isArray(data) && data !== null && data !== undefined) {
+          const rol: IRol = data.find((itemRol) => itemRol.k_Rol === rol_id);
+          setEditData((prev) => ({ ...prev, ...rol }));
+        }
+      })
+      .catch((error) => {
+        console.info(error.message);
+      });
+  }, [rol_id]);
+ */
+
+  useEffect(() => {
+    setLoading(true); // Activar el estado de carga
+
+    getData("linix-roles")
+      .then((data) => {
+        if (Array.isArray(data) && data !== null && data !== undefined) {
+          const rol: IRol = data.find((itemRol) => itemRol.k_Rol === rol_id);
+          setEditData(rol);
+        }
+      })
+      .catch((error) => {
+        console.error(error.message);
+      })
+      .finally(() => {
+        setLoading(false); // Desactivar el estado de carga una vez que se ha completado la obtención de datos
+      });
+  }, [rol_id]);
+
+  const handleTabChange = (tabId: string) => {
+    setSelectedTab(tabId);
+  };
+
+  console.log(editData, "data que se envia para renderizar");
+
+  const valuesGeneralInformation = {
+    roleName: editData.k_Rol,
+    description: editData.n_Uso,
+    aplication: editData.k_Tipcon,
+  };
+
+  console.log(valuesGeneralInformation, "valors para general informacin");
 
   return (
     <EditRoleUI
-    /*       selectedTab={selectedTab}
+      data={valuesGeneralInformation}
+      onTabChange={handleTabChange}
+      selectedTab={selectedTab}
+      dataTabs={Tabs}
+      smallScreen={smallScreen}
+      loading={loading}
+      /*  
       handleTabChange={handleTabChange}
       editData={editData}
       handleSubmit={handleSubmit}
--- src/pages/privileges/outlets/roles/edit-role/interface.tsx
+++ src/pages/privileges/outlets/roles/edit-role/interface.tsx
@@ -1,7 +1,79 @@
-export const EditRoleUI = () => {
-  return (
-    <div>
-      <h1>EditRoleUI</h1>
-    </div>
+import {
+  Button,
+  Stack,
+  SkeletonLine,
+  Breadcrumbs,
+  inube,
+  Tabs,
+} from "@inube/design-system";
+import { PageTitle } from "@src/components/PageTitle";
+import { GeneralInformationForm } from "../add-role/forms/GeneralInformationForm";
+//import { AncillaryAccountsForm } from "../add-role/forms/AncillaryAccounts";
+//import { InitializerForm } from "../../forms/InitializerForm";
+import { editRolConfig, stepsAddRol } from "../add-role/config/addRol.config";
+
+interface ITabs {
+  id: string;
+  label: string;
+  isDisabled: boolean;
+}
+
+interface IEditRoleUIProps {
+  data: any;
+  dataTabs: ITabs[];
+  onTabChange: (tabId: string) => void;
+  selectedTab: string;
+  smallScreen: boolean;
+  loading: boolean;
+}
+
+export const EditRoleUI = (props: IEditRoleUIProps) => {
+  const { data, onTabChange, selectedTab, dataTabs, smallScreen, loading } =
+    props;
+
+  return loading ? (
+    <SkeletonLine animated />
+  ) : (
+    <Stack direction="column" padding={smallScreen ? "s200" : "s400 s800"}>
+      <Stack gap={inube.spacing.s600} direction="column">
+        <Stack gap={inube.spacing.s400} direction="column">
+          <Breadcrumbs crumbs={editRolConfig[0].crumbs} />
+          <Stack
+            justifyContent="space-between"
+            alignItems="center"
+            gap={inube.spacing.s650}
+          >
+            <PageTitle
+              title={editRolConfig[0].title}
+              description={editRolConfig[0].description}
+              navigatePage="/privileges/roles"
+            />
+          </Stack>
+        </Stack>
+        <>
+          <Tabs
+            onChange={onTabChange}
+            selectedTab={selectedTab}
+            tabs={dataTabs}
+          />
+
+          {selectedTab === stepsAddRol.generalInformation.label && (
+            <GeneralInformationForm initialValues={data} />
+          )}
+        </>
+        <Stack gap="16px" justifyContent="flex-end">
+          <Button
+            type="button"
+            spacing="compact"
+            variant="none"
+            appearance="gray"
+          >
+            Cancelar
+          </Button>
+
+          <Button spacing="compact">Guardar</Button>
+        </Stack>
+      </Stack>
+    </Stack>
   );
 };
