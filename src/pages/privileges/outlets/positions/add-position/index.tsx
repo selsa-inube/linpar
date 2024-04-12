6ff4c08d20cdb3b69e6d699862121a4515a66f96
Angie Pinilla <88405711+ANGIECATHERINEP@users.noreply.github.com>
2024-04-11 19:29:04 -0500

    feat: assisted in add positions

---
+++ src/pages/privileges/outlets/positions/add-position/index.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { AddPositionUI } from "./interface";
+import { stepsAddPosition } from "./config/addPosition.config";
+
+const steps = Object.values(stepsAddPosition);
+
+export function AddPosition() {
+  const [currentStep, setCurrentStep] = useState<number>(
+    stepsAddPosition.generalInformation.id
+  );
+
+  const handleStepChange = (stepId: number) => {
+    const changeStepKey = Object.entries(stepsAddPosition).find(
+      ([, config]) => config.id === stepId
+    )?.[0];
+
+    if (!changeStepKey) return;
+
+    setCurrentStep(stepId);
+  };
+
+  const handleNextStep = () => {
+    if (currentStep + 1 <= steps.length) {
+      handleStepChange(currentStep + 1);
+    }
+  };
+
+  const handlePreviousStep = () => {
+    handleStepChange(currentStep - 1);
+  };
+
+  return (
+    <AddPositionUI
+      steps={steps}
+      currentStep={currentStep}
+      handleNextStep={handleNextStep}
+      handlePreviousStep={handlePreviousStep}
+    />
+  );
+}
---
+++ src/pages/privileges/outlets/positions/types.ts
@@ -0,0 +1,5 @@
+export interface IStep {
+  id: number;
+  label: string;
+  description: string;
+}
