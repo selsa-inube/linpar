19e816ef0cb72ee55ee60bb05fc1f7f956d006a5
LinconOssa <lossa@sistemasenlinea.com.co>
2024-04-23 11:49:31 -0500

    fix: adjust the privileges options

--- src/pages/privileges/outlets/options/index.tsx
+++ src/pages/privileges/outlets/options/index.tsx
@@ -1,13 +1,25 @@
 import { PrivilegesOptionsUI } from "./interface";
 import { privilegeOptionsConfig } from "./config/privileges.config";
-import { appsConfig } from "@components/layout/AppPage/config/apps.config";
-
+import { useContext } from "react";
+import { AppContext } from "@src/context/AppContext";
+import { appsConfig } from "@src/components/layout/AppPage/config/apps.config";
 function PrivilegesOptions() {
+  const { user } = useContext(AppContext);
+
+  const allowedOptions =
+    user.company !== "sistemasenlinea"
+      ? ["Usuarios", "Cargos"]
+      : ["Usuarios", "Casos de uso Linix", "Roles", "Cargos"];
+
+  const filteredOptions = privilegeOptionsConfig.filter((option) =>
+    allowedOptions.includes(option.label)
+  );
+
   return (
     <PrivilegesOptionsUI
       appName={appsConfig[0].label}
       appDescription={appsConfig[0].description}
-      appOptions={privilegeOptionsConfig}
+      appOptions={filteredOptions}
       appRoute={appsConfig[0].crumbs}
     />
   );
