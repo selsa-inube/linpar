3fe2d72a4f163d882faa40c40340461786b7f000
LinconOssa <lossa@sistemasenlinea.com.co>
2024-08-01 16:58:45 -0500

    fix: positions servicios

---
+++ src/services/positions/postPositions/mappers.ts
@@ -0,0 +1,13 @@
+import { IPosition } from "@src/pages/privileges/outlets/positions/add-position/types";
+
+const mapPositionsEntityToApi = (
+  position: IPosition
+): Record<string, string | number | object> => {
+  return {
+    k_Grupo: String(position.k_Grupo),
+    n_Grupo: String(position.n_Grupo),
+    n_Uso: String(position.n_Uso),
+  };
+};
+
+export { mapPositionsEntityToApi };
