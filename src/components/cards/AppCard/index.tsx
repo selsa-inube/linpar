4eeb56aabc5edbb26bc44f12fa79d537f253b39f
angie pinilla <angiepinillanova@gmail.com>
2024-09-09 12:05:16 -0500

    feat: create main menu card

---
+++ src/components/cards/AppCard/index.tsx
@@ -0,0 +1,36 @@
+import { Stack } from "@inubekit/stack";
+import { Text } from "@inubekit/text";
+import { Icon } from "@inubekit/icon";
+
+import { StyledAppCard } from "./styles";
+import { tokens } from "@src/design/tokens";
+
+interface AppCardProps {
+  label: string;
+  description: string;
+  icon: JSX.Element;
+  url: string;
+}
+
+function AppCard(props: AppCardProps) {
+  const { label, description, icon, url } = props;
+
+  return (
+    <StyledAppCard to={url}>
+      <Stack direction="column" gap={tokens.spacing.s200}>
+        <Text type="title" size="medium" weight="bold">
+          {label}
+        </Text>
+        <Text type="body" size="small">
+          {description}
+        </Text>
+      </Stack>
+      <Stack justifyContent="flex-end">
+        <Icon icon={icon} appearance="dark" />
+      </Stack>
+    </StyledAppCard>
+  );
+}
+
+export { AppCard };
+export type { AppCardProps };
---
+++ src/components/cards/AppCard/styles.ts
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { inube } from "@inubekit/foundations";
+import { tokens } from "@src/design/tokens";
+
+const StyledAppCard = styled(Link)`
+  box-sizing: border-box;
+  padding: ${tokens.spacing.s300};
+  min-height: 170px;
+  width: 250px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  border-radius: ${tokens.spacing.s050};
+  text-decoration: none;
+  color: ${({ theme }) =>
+    theme?.color?.stroke.dark.regular || inube.palette.neutral.N900};
+  border: 1px solid
+    ${({ theme }) =>
+      theme?.color?.stroke.dark.regular || inube.palette.neutral.N30};
+  box-shadow: 3px 3px 5px 1px
+    ${({ theme }) =>
+      theme?.color?.surface?.gray?.regular || inube.palette.neutral.N30};
+  cursor: pointer;
+
+  &:hover {
+    color: ${({ theme }) =>
+      theme?.color?.surface?.gray?.regular || inube.palette.neutral.N30};
+    background-color: ${({ theme }) =>
+      theme?.color?.surface?.gray?.regular || inube.palette.neutral.N30};
+    box-shadow: none;
+  }
+
+  @media (max-width: 400px) {
+    padding: ${tokens.spacing.s200};
+    width: 100%;
+    min-height: 100px;
+    gap: ${tokens.spacing.s100};
+
+    div {
+      gap: ${tokens.spacing.s050};
+    }
+  }
+`;
+
+export { StyledAppCard };
