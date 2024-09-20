0643ee78d45fa114aaad2ca354c7b0083b5244f2
angie pinilla <angiepinillanova@gmail.com>
2024-09-20 15:04:30 -0500

    feat: add logo in footer of home

--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -5,6 +5,8 @@ import { inube } from "@inubekit/foundations";
 import { tokens } from "@design/tokens";
 
 const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
   box-sizing: border-box;
   padding-bottom: ${tokens.spacing.s500};
   height: 100vh;
@@ -40,17 +42,16 @@ const StyledLogo = styled.img`
 `;
 
 const StyledTitle = styled.div`
-  padding: ${tokens.spacing.s400} ${tokens.spacing.s1600} ${tokens.spacing.s800};
+  padding: ${tokens.spacing.s400} ${tokens.spacing.s1600} ${tokens.spacing.s600};
 
   @media screen and (max-width: 805px) {
     padding: ${tokens.spacing.s400} ${tokens.spacing.s1000}
-      ${tokens.spacing.s800};
+      ${tokens.spacing.s500};
   }
 `;
 
 const StyledContainerCards = styled.div`
   box-sizing: border-box;
-  max-width: 1400px;
   padding: ${tokens.spacing.s0} ${tokens.spacing.s1400} ${tokens.spacing.s400}
     170px;
   display: flex;
@@ -64,6 +65,11 @@ const StyledContainerCards = styled.div`
   }
 `;
 
+const StyledFooter = styled.footer`
+  margin-top: auto;
+  padding: ${tokens.spacing.s0} ${tokens.spacing.s1600} ${tokens.spacing.s0};
+`;
+
 export {
   StyledContainer,
   StyledHeaderContainer,
@@ -72,4 +78,5 @@ export {
   StyledContentImg,
   StyledLogo,
   StyledContainerCards,
+  StyledFooter,
 };
