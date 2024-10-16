21badf23cef7320c0a7a11072bd18ba5b328d6c0
LinconOssa <lossa@sistemasenlinea.com.co>
2024-10-16 09:59:10 -0500

    fix: ajustes rama

--- src/components/layout/ErrorPage/index.tsx
+++ src/components/layout/ErrorPage/index.tsx
@@ -5,6 +5,7 @@ import { useMediaQueries } from "@inubekit/hooks";
 import selsaLogo from "@assets/images/selsa.png";
 import errorImage from "@assets/images/timeout.png";
 import { Stack } from "@inubekit/stack";
+import { useAuth0 } from "@auth0/auth0-react";
 
 interface ErrorPageProps {
   logo?: string;
@@ -27,9 +28,10 @@ function ErrorPage(props: ErrorPageProps) {
 
   const mediaQueries = ["(max-width: 1000px)", "(max-width: 600px)"];
   const matches = useMediaQueries(mediaQueries);
-
+  const { logout } = useAuth0();
   const handleRedirect = () => {
-    window.location.href = "https://www.google.com";
+    localStorage.clear();
+    logout({ logoutParams: { returnTo: "https://www.google.com" } });
   };
 
   return (
