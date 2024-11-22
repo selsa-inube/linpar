3ba14059481e6a845445bdd69d4fe6f26982cc0b
LinconOssa <lossa@sistemasenlinea.com.co>
2024-11-22 17:56:54 -0500

    fix: servicios

---
+++ src/services/signupInvitations/mappers.ts
@@ -0,0 +1,17 @@
+import { IUsersSignupInvitation } from "./types";
+
+const mapSignupInvitationEntityToApi = (
+  signupInvitation: IUsersSignupInvitation
+): Record<string, string | number | object> => {
+  return {
+    email: String(signupInvitation.email),
+    invitationId: String(signupInvitation.invitationId),
+    modifyJustification: String(signupInvitation.modifyJustification),
+    password: String(signupInvitation.password),
+    phoneNumber: String(signupInvitation.phoneNumber),
+    userAccountId: String(signupInvitation.userAccountId),
+    userName: String(signupInvitation.userName),
+  };
+};
+
+export { mapSignupInvitationEntityToApi };
